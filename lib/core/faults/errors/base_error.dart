2986a1a40b3119942b4e0ac33d1bf27fb3fbfb7b
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-03-25 16:15:50 -0300

    Adds `BaseError`

---
+++ lib/core/faults/errors/base_error.dart
@@ -0,0 +1,33 @@
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+/// Core [Error] class - every class that represent an [Error] should extend from [BaseError].
+///
+/// The difference between throwing an [Error] and an [Exception] can be found in their respective declarations in
+/// `dart:core`.
+@immutable
+abstract class BaseError extends Error with EquatableMixin {
+  BaseError({required this.type, required this.message});
+
+  final ErrorType type;
+  final String message;
+
+  @override
+  List<Object> get props => [type];
+
+  @override
+  String toString() => '$type - $message';
+}
+
+enum ErrorType {
+  // InconsistentStateError
+  inconsistentState,
+  coordinatorInconsistentState,
+  serviceInconsistentState,
+  repositoryInconsistentState,
+  viewModelInconsistentState,
+  layoutInconsistentState,
+
+  // SerializationError
+  serialization,
+}
