2a23de154d71126b61d7cabc4de7aff0d094f408
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-04-10 12:43:07 -0300

    Adds `constants/spacings` implementation for the `layoutr` package

---
+++ lib/application/constants/spacings.dart
@@ -0,0 +1,17 @@
+import 'package:layoutr/common_layout.dart';
+
+/// Raw values for the [Spacing]
+///
+/// The values below are used for all device's sizes, meaning that this application is not implementing custom spacings
+/// for devices with different ratios/sizes.
+///
+/// - `xxxSmall = 2`
+/// - `xxSmall = 4`
+/// - `xSmall = 8`
+/// - `small = 12`
+/// - `medium = 16`
+/// - `large = 24`
+/// - `xLarge = 32`
+/// - `xxLarge = 40`
+/// - `xxxLarge = 48`
+const spacings = RawSpacings(2, 4, 8, 12, 16, 24, 32, 40, 48);
