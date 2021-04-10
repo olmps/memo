cfc86a52875378cf35e1effd9d077a9217bf50bc
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-04-10 15:57:20 -0300

    Fixes wrong variable names

--- lib/application/theme/memo_theme_colors.dart
+++ lib/application/theme/memo_theme_colors.dart
@@ -8,16 +8,16 @@ import 'package:flutter/material.dart';
 // MemoTheme.classic
 //
 MaterialColor buildClassicPrimarySwatch() {
-  const _primary = Color(0xFF4DBB74);
+  const defaultPrimary = Color(0xFF4DBB74);
   return MaterialColor(
-    _primary.value,
+    defaultPrimary.value,
     const {
       50: Color(0xFFF1FFF6),
       100: Color(0xFFCAFFDD),
       200: Color(0xFFA2FFC3),
       300: Color(0xFF75F5A3),
       400: Color(0xFF60D88B),
-      500: _primary,
+      500: defaultPrimary,
       600: Color(0xFF2C814A),
       700: Color(0xFF1F6337),
       800: Color(0xFF134625),
@@ -27,16 +27,16 @@ MaterialColor buildClassicPrimarySwatch() {
 }
 
 MaterialColor buildClassicSecondarySwatch() {
-  const _primary = Color(0xFF846BCD);
+  const defaultSecondary = Color(0xFF846BCD);
   return MaterialColor(
-    _primary.value,
+    defaultSecondary.value,
     const {
       50: Color(0xFFF7F4FF),
       100: Color(0xFFDFD4FF),
       200: Color(0xFFC7B3FF),
       300: Color(0xFFAF93FF),
       400: Color(0xFF9C81EA),
-      500: _primary,
+      500: defaultSecondary,
       600: Color(0xFF6E57B0),
       700: Color(0xFF594593),
       800: Color(0xFF453475),
@@ -46,16 +46,16 @@ MaterialColor buildClassicSecondarySwatch() {
 }
 
 MaterialColor buildClassicNeutralSwatch() {
-  const _primaryNeutral = Color(0xFF7A748E);
+  const defaultNeutral = Color(0xFF7A748E);
   return MaterialColor(
-    _primaryNeutral.value,
+    defaultNeutral.value,
     const {
       50: Color(0xFFF7F7F9),
       100: Color(0xFFEDEBF6),
       200: Color(0xFFC7C3DB),
       300: Color(0xFFADA7C1),
       400: Color(0xFF7A748E),
-      500: _primaryNeutral,
+      500: defaultNeutral,
       600: Color(0xFF615D75),
       700: Color(0xFF4A465B),
       800: Color(0xFF343142),
