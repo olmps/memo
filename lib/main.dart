b5cc542ad23ecd0d3ab03fe0f0e85c6bd2bbd037
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-04-10 12:44:33 -0300

    Adds `updateLicenseRegistry` call in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+
 import 'package:memo/application/app.dart';
+import 'package:memo/application/utils/license_update.dart';
 import 'package:memo/application/view-models/app_vm.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+
+  updateLicenseRegistry();
 
   final appVM = AppVMImpl();
   runApp(AppRoot(appVM));
