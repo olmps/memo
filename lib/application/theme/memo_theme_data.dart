67594f759661d06e36068bf2d04bb8dc9b5e72fd
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-04-10 15:57:08 -0300

    Fixes typo

--- lib/application/theme/memo_theme_data.dart
+++ lib/application/theme/memo_theme_data.dart
@@ -8,7 +8,7 @@ enum MemoTheme { classic }
 /// `ThemeData` doesn't support custom properties. While there are workarounds, they don't quite work for applications
 /// that require multiple sub-themes.
 ///
-/// There is an [open issue](https://github.com/flutter/flutter/issues/31522) in flutter's frameowkr, tracking this
+/// There is an [open issue](https://github.com/flutter/flutter/issues/31522) in flutter's framework, tracking this
 /// missing feature.
 class MemoThemeData {
   const MemoThemeData(
