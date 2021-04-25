9f65d5d8eaff5248aec4c115e27d08c94240f5bd
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-04-25 16:42:10 -0300

    Updates tests to conform to the model updates

--- test/fixtures/fixtures.dart
+++ test/fixtures/fixtures.dart
@@ -11,3 +11,4 @@ Map<String, dynamic> memoBlock() => _readFixture('memo_block.json');
 Map<String, dynamic> memo() => _readFixture('memo.json');
 Map<String, dynamic> collection() => _readFixture('collection.json');
 Map<String, dynamic> resource() => _readFixture('resource.json');
+Map<String, dynamic> user() => _readFixture('user.json');
