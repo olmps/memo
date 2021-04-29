8b0676f1a8e2b2b2fe696b927431e212b59cc431
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-04-29 17:26:58 -0300

    Adds new sembast functionalities tests

--- test/data/gateways/sembast_database_test.dart
+++ test/data/gateways/sembast_database_test.dart
@@ -6,8 +6,9 @@ import 'package:sembast/sembast_memory.dart';
 void main() {
   final fakeObject = {'fake': 'fake'};
   const fakeRecordId = 'a-fake-id';
-  const fakeStore = 'fake_store';
-  final fakeRecord = stringMapStoreFactory.store(fakeStore).record(fakeRecordId);
+  const fakeRawStore = 'fake_store';
+  final fakeStore = stringMapStoreFactory.store(fakeRawStore);
+  final fakeRecord = fakeStore.record(fakeRecordId);
 
   late Database memorySembast;
   late SembastDatabase db;
@@ -19,11 +20,21 @@ void main() {
   });
 
   test('DatabaseRepositoryImpl should put a new object', () async {
-    expect(await fakeRecord.get(memorySembast), null);
+    expect(await fakeRecord.get(memorySembast), isNull);
 
-    await db.put(id: fakeRecordId, object: fakeObject, store: fakeStore);
+    await db.put(id: fakeRecordId, object: fakeObject, store: fakeRawStore);
 
     expect(await fakeRecord.get(memorySembast), fakeObject);
+  });
+
+  test('DatabaseRepositoryImpl should put multiple objects at once', () async {
+    expect(await fakeRecord.get(memorySembast), isNull);
+
+    final records = [fakeRecordId, 'second-$fakeRecordId'];
+    final fakeObjects = [fakeObject, fakeObject];
+    await db.putAll(ids: records, objects: fakeObjects, store: fakeRawStore);
+
+    expect(await fakeStore.records(records).get(memorySembast), fakeObjects);
   });
 
   test('DatabaseRepositoryImpl should update an existing object', () async {
@@ -31,7 +42,7 @@ void main() {
 
     final fakeUpdatedObject = {'fake': 'fakeUpdated', 'newFake': 'fake'};
 
-    await db.put(id: fakeRecordId, object: fakeUpdatedObject, store: fakeStore);
+    await db.put(id: fakeRecordId, object: fakeUpdatedObject, store: fakeRawStore);
 
     expect(await fakeRecord.get(memorySembast), fakeUpdatedObject);
   });
@@ -41,7 +52,7 @@ void main() {
 
     final fakeUpdatedObject = {'newFake': 'fake'};
 
-    await db.put(id: fakeRecordId, object: fakeUpdatedObject, store: fakeStore, shouldMerge: false);
+    await db.put(id: fakeRecordId, object: fakeUpdatedObject, store: fakeRawStore, shouldMerge: false);
 
     expect(await fakeRecord.get(memorySembast), fakeUpdatedObject);
   });
@@ -51,7 +62,7 @@ void main() {
 
     final fakeUpdatedObject = {'newFake': 'fake'};
 
-    await db.put(id: fakeRecordId, object: fakeUpdatedObject, store: fakeStore);
+    await db.put(id: fakeRecordId, object: fakeUpdatedObject, store: fakeRawStore);
 
     fakeUpdatedObject.addAll(fakeObject);
     expect(await fakeRecord.get(memorySembast), fakeUpdatedObject);
@@ -61,44 +72,66 @@ void main() {
     await fakeRecord.put(memorySembast, fakeObject);
 
     expect(await fakeRecord.get(memorySembast), fakeObject);
-    await db.remove(id: fakeRecordId, store: fakeStore);
-    expect(await fakeRecord.get(memorySembast), null);
+    await db.remove(id: fakeRecordId, store: fakeRawStore);
+    expect(await fakeRecord.get(memorySembast), isNull);
+
+    expect(await fakeRecord.get(memorySembast), isNull);
+  });
+
+  test('DatabaseRepositoryImpl should remove multiple existing objects at once', () async {
+    final records = [fakeRecordId, 'second-$fakeRecordId'];
+    final fakeObjects = [fakeObject, fakeObject];
+    await fakeStore.records(records).put(memorySembast, fakeObjects);
+
+    expect(await fakeStore.records(records).get(memorySembast), fakeObjects);
+    await db.removeAll(ids: records, store: fakeRawStore);
+
+    expect(await fakeStore.find(memorySembast), const <dynamic>[]);
   });
 
   test('DatabaseRepositoryImpl should do nothing when removing a nonexistent object', () async {
     await fakeRecord.put(memorySembast, fakeObject);
 
-    await db.remove(id: fakeRecordId, store: fakeStore);
-    expect(await fakeRecord.get(memorySembast), null);
+    await db.remove(id: fakeRecordId, store: fakeRawStore);
+    expect(await fakeRecord.get(memorySembast), isNull);
   });
 
   test('DatabaseRepositoryImpl should retrieve a single existing object', () async {
     await fakeRecord.put(memorySembast, fakeObject);
 
-    final object = await db.get(id: fakeRecordId, store: fakeStore);
+    final object = await db.get(id: fakeRecordId, store: fakeRawStore);
     expect(object, isNotNull);
   });
 
+  test('DatabaseRepositoryImpl should retrieve multiple existing objects by their ids', () async {
+    final records = [fakeRecordId, 'second-$fakeRecordId'];
+    final fakeObjects = [fakeObject, fakeObject];
+    await fakeStore.records(records).put(memorySembast, fakeObjects);
+
+    final objects = await db.getAllByIds(ids: records, store: fakeRawStore);
+    expect(objects, fakeObjects);
+  });
+
   test('DatabaseRepositoryImpl should get null when retrieving a single nonexistent object', () async {
-    final object = await db.get(id: fakeRecordId, store: fakeStore);
-    expect(object, null);
+    final object = await db.get(id: fakeRecordId, store: fakeRawStore);
+    expect(object, isNull);
   });
 
   test('DatabaseRepositoryImpl should retrieve multiple objects', () async {
     await fakeRecord.put(memorySembast, fakeObject);
-    await stringMapStoreFactory.store(fakeStore).record('2').put(memorySembast, fakeObject);
+    await stringMapStoreFactory.store(fakeRawStore).record('2').put(memorySembast, fakeObject);
 
-    final objects = await db.getAll(store: fakeStore);
+    final objects = await db.getAll(store: fakeRawStore);
     expect(objects.length, 2);
   });
 
   test('DatabaseRepositoryImpl should retrieve an empty list if there is no objects in the store', () async {
-    final objects = await db.getAll(store: fakeStore);
+    final objects = await db.getAll(store: fakeRawStore);
     expect(objects.isEmpty, true);
   });
 
   test('DatabaseRepositoryImpl should emit new events when listening to store updates', () async {
-    final stream = await db.listenAll(store: fakeStore);
+    final stream = await db.listenAll(store: fakeRawStore);
 
     final expectedEmissions = <List<Map<String, String>>>[
       [], // First emission is the "onListen", which is an empty store
@@ -109,6 +142,6 @@ void main() {
     expect(stream, emitsInOrder(expectedEmissions));
 
     await fakeRecord.put(memorySembast, fakeObject);
-    await stringMapStoreFactory.store(fakeStore).record('other-fake-id').put(memorySembast, fakeObject);
+    await stringMapStoreFactory.store(fakeRawStore).record('other-fake-id').put(memorySembast, fakeObject);
   });
 }
