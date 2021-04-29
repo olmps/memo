ef72ead05444c240299068a29a0abd9c8db4a0e4
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-04-29 12:27:37 -0300

    Adds `getCollectionMemosByIds` and `putCollectionsWithCollectionMemos` to `CollectionRepository`

--- lib/data/repositories/collection_repository.dart
+++ lib/data/repositories/collection_repository.dart
@@ -1,10 +1,14 @@
 import 'dart:async';
 
 import 'package:memo/core/faults/errors/inconsistent_state_error.dart';
+import 'package:memo/data/gateways/application_bundle.dart';
 import 'package:memo/data/gateways/sembast_database.dart';
 import 'package:memo/data/serializers/collection_serializer.dart';
+import 'package:memo/data/serializers/collection_memos_serializer.dart';
+import 'package:memo/data/serializers/memo_difficulty_parser.dart';
 import 'package:memo/domain/enums/memo_difficulty.dart';
 import 'package:memo/domain/models/collection.dart';
+import 'package:memo/domain/transients/collection_memos.dart';
 
 /// Handles all read, write and serialization operations pertaining to one or multiple [Collection]
 abstract class CollectionRepository {
@@ -12,10 +16,18 @@ abstract class CollectionRepository {
   Future<Collection> getCollection({required String id});
 
   /// Retrieves all available [Collection] and keeps listening to any changes made to them
-  Future<List<Stream<Collection>>> listenToAllCollections();
+  Future<Stream<List<Collection>>> listenToAllCollections();
 
   /// Retrieves all available [Collection]
   Future<List<Collection>> getAllCollections();
+
+  /// Retrieves all [CollectionMemos], matched by the respective ([ids])
+  Future<List<CollectionMemos>> getCollectionMemosByIds(List<String> ids);
+
+  /// Put all [Collection] with a list of [CollectionMemos]
+  ///
+  /// All [collections] match their respective [Collection] using the [CollectionMetadata.id] - if already existing
+  Future<void> putCollectionsWithCollectionMemos(List<CollectionMemos> collections);
 
   /// Updates a [Collection] (of [id]) with the execution-related arguments
   ///
@@ -31,18 +43,17 @@ abstract class CollectionRepository {
   });
 }
 
-final _collectionSerializer = CollectionSerializer();
-final _collectionTransformer = StreamTransformer<Map<String, dynamic>, Collection>.fromHandlers(
-  handleData: (rawCollection, sink) {
-    sink.add(_collectionSerializer.from(rawCollection));
-  },
-);
-
 class CollectionRepositoryImpl implements CollectionRepository {
-  CollectionRepositoryImpl(this._db);
+  CollectionRepositoryImpl(this._db, this._appBundle);
 
   final SembastDatabase _db;
   final _collectionStore = 'collections';
+
+  final ApplicationBundle _appBundle;
+  final _collectionsRoot = 'assets/collections';
+  final _collectionsMemosSerializer = CollectionMemosSerializer();
+
+  final _collectionSerializer = CollectionSerializer();
 
   @override
   Future<Collection> getCollection({required String id}) async {
@@ -62,17 +73,39 @@ class CollectionRepositoryImpl implements CollectionRepository {
   }
 
   @override
-  Future<List<Stream<Collection>>> listenToAllCollections() async {
-    final rawCollections = await _db.getAll(store: _collectionStore);
-    final collectionIds = rawCollections.map((rawCollection) => rawCollection[CollectionKeys.id] as String).toList();
+  Future<List<CollectionMemos>> getCollectionMemosByIds(List<String> ids) async {
+    final collectionsFutures = ids.map((id) => _appBundle.loadJson('$_collectionsRoot/$id.json'));
+    final rawCollections = await Future.wait<dynamic>(collectionsFutures);
 
-    final listenFutures =
-        collectionIds.map((collectionId) => _db.listenTo(id: collectionId, store: _collectionStore)).toList();
-    final rawCollectionListeners = await Future.wait(listenFutures);
+    final castCollections = List<Map<String, dynamic>>.from(rawCollections);
+    return castCollections.map(_collectionsMemosSerializer.from).toList();
+  }
 
-    return rawCollectionListeners
-        .map((collectionStream) => collectionStream.transform(_collectionTransformer))
-        .toList();
+  @override
+  Future<void> putCollectionsWithCollectionMemos(List<CollectionMemos> collections) async {
+    return _db.putAll(
+      ids: collections.map((collection) => collection.id).toList(),
+      objects: collections
+          .map(
+            (collection) => {
+              CollectionKeys.id: collection.id,
+              CollectionKeys.name: collection.name,
+              CollectionKeys.description: collection.description,
+              CollectionKeys.category: collection.category,
+              CollectionKeys.tags: collection.tags,
+              CollectionKeys.uniqueMemosAmount: collection.uniqueMemosAmount,
+              CollectionKeys.uniqueMemoExecutionsAmount: collection.uniqueMemoExecutionsAmount,
+            },
+          )
+          .toList(),
+      store: _collectionStore,
+    );
+  }
+
+  @override
+  Future<Stream<List<Collection>>> listenToAllCollections() async {
+    final rawCollections = await _db.listenAll(store: _collectionStore);
+    return rawCollections.map((event) => event.map(_collectionSerializer.from).toList());
   }
 
   @override
@@ -85,7 +118,7 @@ class CollectionRepositoryImpl implements CollectionRepository {
       _db.put(
         id: id,
         object: <String, dynamic>{
-          CollectionKeys.executionsAmounts: executionsAmounts,
+          CollectionKeys.executionsAmounts: executionsAmounts.map((key, value) => MapEntry(key.raw, value)),
           CollectionKeys.timeSpentInMillis: timeSpentInMillis,
           if (uniqueExecutionsAmount != null) CollectionKeys.uniqueMemoExecutionsAmount: uniqueExecutionsAmount,
         },
