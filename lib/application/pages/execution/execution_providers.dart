241d06a2497f7afbf0063ae587415b77e05c82df
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-04-29 15:35:42 -0300

    Adds confusing/missing docs un `executionCollectionId` provider

--- lib/application/pages/execution/execution_providers.dart
+++ lib/application/pages/execution/execution_providers.dart
@@ -3,9 +3,10 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:memo/application/view-models/execution/collection_execution_vm.dart';
 
-/// Provides a overridable current to with a [ScopedProvider]
+/// Provides an overridable collection id to be used in the scope of a collection execution
 ///
-/// There is no default value (meaning `null`) for this provider, it must always be supplied to any widget that uses it
+/// There is no default value (meaning `null`) for this [ScopedProvider], it must always be supplied to any widget that
+/// uses it
 final executionCollectionId = ScopedProvider<String>(null);
 
 /// Syntax sugar for **reading** the current [CollectionExecutionVM] instance
