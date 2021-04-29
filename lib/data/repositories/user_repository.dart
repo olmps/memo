a04237dec638dca2b56e1ac3cccd6efca72bf500
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-04-29 12:37:04 -0300

    Updates `UserRepository` with `createUser`, `getLastCollectionsVersions`, `updateCollectionsVersions` and `updateMemoExecutionChunkGoal`

--- lib/data/repositories/user_repository.dart
+++ lib/data/repositories/user_repository.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:memo/core/faults/errors/inconsistent_state_error.dart';
 import 'package:memo/data/gateways/sembast_database.dart';
 import 'package:memo/data/serializers/user_serializer.dart';
+import 'package:memo/data/serializers/memo_difficulty_parser.dart';
 import 'package:memo/domain/enums/memo_difficulty.dart';
 import 'package:memo/domain/models/user.dart';
 
@@ -12,7 +13,21 @@ abstract class UserRepository {
   Future<Stream<User>> listenToUser();
 
   /// Retrieves the current [User]
-  Future<User> getUser();
+  Future<User?> getUser();
+
+  /// Creates a new pristine [User]
+  Future<void> createUser();
+
+  /// Retrieves the last stored versions per collection given the current [User]
+  ///
+  /// Returns a `Map` that associates a `Collection` name as its key and its version as the value
+  Future<Map<String, int>?> getLastCollectionsVersions();
+
+  /// Updates the versions per collection given the current [User]
+  Future<void> updateCollectionsVersions(Map<String, int> collectionsVersions);
+
+  /// Sets the current [User]
+  Future<void> updateMemoExecutionChunkGoal(int goal);
 
   /// Updates the [User] with the execution-related arguments
   ///
@@ -21,40 +36,64 @@ abstract class UserRepository {
   Future<void> updateExecution({required Map<MemoDifficulty, int> executionsAmounts, required int timeSpentInMillis});
 }
 
-final _userSerializer = UserSerializer();
-final _userStreamTransformer = StreamTransformer<Map<String, Object>, User>.fromHandlers(handleData: (rawUser, sink) {
-  sink.add(_userSerializer.from(rawUser));
-});
-
 class UserRepositoryImpl implements UserRepository {
   UserRepositoryImpl(this._db);
 
   final SembastDatabase _db;
   final _mainStore = '';
   final _userRecord = 'user';
+  final _collectionsVersionsRecord = 'collections_versions';
+
+  final _userSerializer = UserSerializer();
 
   @override
   Future<Stream<User>> listenToUser() async {
     final rawUser = await _db.listenTo(id: _userRecord, store: _mainStore);
-    return rawUser.transform(_userStreamTransformer);
+    return rawUser.map((user) {
+      if (user == null) {
+        throw InconsistentStateError.repository('Missing required user');
+      }
+
+      return _userSerializer.from(user);
+    });
   }
 
   @override
-  Future<User> getUser() async {
+  Future<User?> getUser() async {
     final rawUser = await _db.get(id: _userRecord, store: _mainStore);
-    if (rawUser == null) {
-      throw InconsistentStateError.repository('Missing required user (of record "$_userRecord") in main store');
-    }
-
-    return _userSerializer.from(rawUser);
+    return rawUser != null ? _userSerializer.from(rawUser) : null;
   }
+
+  @override
+  Future<void> createUser() async {
+    const executionsGoal = 10;
+    final encodedUser = _userSerializer.to(User(memosExecutionChunkGoal: executionsGoal));
+    await _db.put(id: _userRecord, object: encodedUser, store: _mainStore);
+  }
+
+  @override
+  Future<Map<String, int>?> getLastCollectionsVersions() async {
+    final rawVersions = await _db.get(id: _collectionsVersionsRecord, store: _mainStore);
+    return rawVersions != null ? Map<String, int>.from(rawVersions) : null;
+  }
+
+  @override
+  Future<void> updateCollectionsVersions(Map<String, int> collectionsVersions) =>
+      _db.put(id: _collectionsVersionsRecord, object: collectionsVersions, store: _mainStore);
+
+  @override
+  Future<void> updateMemoExecutionChunkGoal(int goal) => _db.put(
+        id: _userRecord,
+        object: <String, dynamic>{UserKeys.memosExecutionChunkGoal: goal},
+        store: _mainStore,
+      );
 
   @override
   Future<void> updateExecution({required Map<MemoDifficulty, int> executionsAmounts, required int timeSpentInMillis}) =>
       _db.put(
         id: _userRecord,
         object: <String, dynamic>{
-          UserKeys.executionsAmounts: executionsAmounts,
+          UserKeys.executionsAmounts: executionsAmounts.map((key, value) => MapEntry(key.raw, value)),
           UserKeys.timeSpentInMillis: timeSpentInMillis,
         },
         store: _mainStore,
