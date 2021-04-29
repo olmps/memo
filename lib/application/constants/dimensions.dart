12442622a4104e6dc203dadcc7303b97ca2690b8
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-04-29 13:08:51 -0300

    Adds executions dimensions

--- lib/application/constants/dimensions.dart
+++ lib/application/constants/dimensions.dart
@@ -25,6 +25,21 @@ const double progressCircularProgressSize = 80;
 const double progressCircularProgressLineWidth = 8;
 
 //
+// ExecutionsPage
+//
+const executionsTerminalBorderRadius = BorderRadius.all(Radius.circular(12));
+const double executionsTerminalBorderWidth = 2;
+const double executionsTerminalActionDiameter = 12;
+const double executionsTerminalFadeHeight = 64;
+const double executionsTerminalActionSize = 72;
+const double executionsTerminalActionEmojiTextSize = 40;
+const double executionsTerminalBlur = 32;
+
+const double executionsCompletionDividerHeight = 1;
+
+const double executionsHeaderEmojiTextSize = 80;
+
+//
 // Animations
 //
 const defaultAnimationCurve = Curves.easeInOutCubic;
