6691d742e3357414c17233dff0d9a6515531f83c
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-04-29 12:58:31 -0300

    Updates `CollectionServices`

--- lib/domain/services/collection_services.dart
+++ lib/domain/services/collection_services.dart
@@ -1,23 +1,26 @@
 import 'package:memo/data/repositories/collection_repository.dart';
 import 'package:memo/data/repositories/memo_repository.dart';
-import 'package:memo/domain/isolated_services/memory_stability_services.dart';
+import 'package:memo/domain/isolated_services/memory_recall_services.dart';
 import 'package:memo/domain/models/collection.dart';
 import 'package:memo/domain/transients/collection_status.dart';
 
 /// Handles all domain-specific operations pertaining to one or multiple [Collection]
 abstract class CollectionServices {
   /// Retrieves all available [CollectionStatus] and keeps listening to any changes made to them
-  Future<List<Stream<CollectionStatus>>> listenToAllCollectionsStatus();
+  Future<Stream<List<CollectionStatus>>> listenToAllCollectionsStatus();
 
   /// Retrieves all available [Collection]
   Future<List<CollectionStatus>> getAllCollectionsStatus();
 
-  /// Retrieves the [Collection] (with [collectionId]) current memory stability
+  /// Retrieves the [Collection] (with [collectionId]) current memory recall ratio
   ///
-  /// This value ranges from `0` to `1`, meaning lesser the value, worse is the memory stability.
+  /// This value ranges from `0` to `1`, meaning lesser the value, worse is the memory recall.
   ///
   /// If [Collection.isPristine], this will return `0`.
-  Future<double> getCollectionMemoryStability({required String collectionId});
+  Future<double> getCollectionMemoryRecall({required String collectionId});
+
+  /// Retrieves a [Collection] with the following [id]
+  Future<Collection> getCollectionById(String id);
 }
 
 class CollectionServicesImpl implements CollectionServices {
@@ -26,15 +29,18 @@ class CollectionServicesImpl implements CollectionServices {
   final CollectionRepository collectionRepo;
   final MemoRepository memoRepo;
 
-  final MemoryStabilityServices memoryServices;
+  final MemoryRecallServices memoryServices;
 
   @override
-  Future<List<Stream<CollectionStatus>>> listenToAllCollectionsStatus() async {
+  Future<Stream<List<CollectionStatus>>> listenToAllCollectionsStatus() async {
     final collectionsStream = await collectionRepo.listenToAllCollections();
-    return collectionsStream
-        // Asynchronously transform the stream due to the async calculations
-        .map((collectionStream) => collectionStream.asyncMap(_mapCollectionToCollectionStatus))
-        .toList();
+    // Asynchronously transform the stream due to the async calculations
+    return collectionsStream.asyncMap(
+      (collections) {
+        final mappedStatuses = collections.map(_mapCollectionToCollectionStatus).toList();
+        return Future.wait(mappedStatuses);
+      },
+    );
   }
 
   @override
@@ -45,29 +51,32 @@ class CollectionServicesImpl implements CollectionServices {
   }
 
   @override
-  Future<double> getCollectionMemoryStability({required String collectionId}) =>
-      _getMemosAverageMemoryStability(collectionId: collectionId);
+  Future<double> getCollectionMemoryRecall({required String collectionId}) =>
+      _getMemosAverageMemoryRecall(collectionId: collectionId);
 
-  Future<double> _getMemosAverageMemoryStability({required String collectionId}) async {
+  Future<double> _getMemosAverageMemoryRecall({required String collectionId}) async {
     final allCollectionMemos = await memoRepo.getAllMemos(collectionId: collectionId);
 
-    var averageStability = 0.0;
+    var averageRecall = 0.0;
     allCollectionMemos.forEach((memo) {
-      final stability = memoryServices.evaluateMemoryRecall(memo);
-      if (stability != null) {
-        averageStability += stability;
+      final recall = memoryServices.evaluateMemoryRecall(memo);
+      if (recall != null) {
+        averageRecall += recall;
       }
     });
 
-    return averageStability / allCollectionMemos.length;
+    return averageRecall / allCollectionMemos.length;
   }
 
   Future<CollectionStatus> _mapCollectionToCollectionStatus(Collection collection) async {
-    double? memoryStability;
+    double? memoryRecall;
     if (collection.isCompleted) {
-      memoryStability = await _getMemosAverageMemoryStability(collectionId: collection.id);
+      memoryRecall = await _getMemosAverageMemoryRecall(collectionId: collection.id);
     }
 
-    return CollectionStatus(collection, memoryStability);
+    return CollectionStatus(collection, memoryRecall);
   }
+
+  @override
+  Future<Collection> getCollectionById(String id) => collectionRepo.getCollection(id: id);
 }
