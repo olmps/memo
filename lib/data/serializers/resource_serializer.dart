d3ce7cbee9b50f46982959d6f35af0c59b3c5964
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-05-04 09:51:22 -0300

    Updates `resource` serializer with typed keys

--- lib/data/serializers/resource_serializer.dart
+++ lib/data/serializers/resource_serializer.dart
@@ -2,19 +2,26 @@ import 'package:memo/data/serializers/serializer.dart';
 import 'package:memo/domain/enums/resource_type.dart';
 import 'package:memo/domain/models/resource.dart';
 
+class ResourceKeys {
+  static const id = 'id';
+  static const description = 'description';
+  static const type = 'type';
+  static const url = 'url';
+  static const tags = 'tags';
+}
+
 class ResourceSerializer implements Serializer<Resource, Map<String, dynamic>> {
   @override
   Resource from(Map<String, dynamic> json) {
-    final id = json['id'] as String;
-    final description = json['description'] as String;
-    final rawType = json['type'] as String;
+    final id = json[ResourceKeys.id] as String;
+    final description = json[ResourceKeys.description] as String;
+    final rawType = json[ResourceKeys.type] as String;
     final type = _typeFromRaw(rawType);
 
-    final url = json['url'] as String;
+    final url = json[ResourceKeys.url] as String;
 
-    final rawTags = json['tags'] as List;
-    // Casting just to make sure, because sembast returns an ImmutableList<dynamic>
-    final tags = rawTags.cast<String>();
+    final rawTags = json[ResourceKeys.tags] as List;
+    final tags = List<String>.from(rawTags);
 
     return Resource(
       id: id,
@@ -27,11 +34,11 @@ class ResourceSerializer implements Serializer<Resource, Map<String, dynamic>> {
 
   @override
   Map<String, dynamic> to(Resource resource) => <String, dynamic>{
-        'id': resource.id,
-        'description': resource.description,
-        'tags': resource.tags,
-        'type': resource.type.raw,
-        'url': resource.url,
+        ResourceKeys.id: resource.id,
+        ResourceKeys.description: resource.description,
+        ResourceKeys.tags: resource.tags,
+        ResourceKeys.type: resource.type.raw,
+        ResourceKeys.url: resource.url,
       };
 
   ResourceType _typeFromRaw(String raw) => ResourceType.values.firstWhere(
