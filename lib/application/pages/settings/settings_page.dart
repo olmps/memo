80e5545dacab5d419604b11421808f495278c0ba
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-05-04 12:47:50 -0300

    Adds `SettingsPage` and `SettingsVM`

--- lib/application/pages/settings/settings_page.dart
+++ lib/application/pages/settings/settings_page.dart
@@ -1,11 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:layoutr/common_layout.dart';
+import 'package:memo/application/constants/strings.dart' as strings;
+import 'package:memo/application/theme/theme_controller.dart';
+import 'package:memo/application/view-models/settings/settings_vm.dart';
+import 'package:memo/application/widgets/theme/link_button.dart';
+import 'package:memo/core/faults/errors/inconsistent_state_error.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends HookWidget {
   @override
   Widget build(BuildContext context) {
+    final items = useProvider(settingsVM).items;
+    final licenseBg = useTheme().neutralSwatch.shade900;
+
     return Scaffold(
-      appBar: AppBar(title: const Text('Settings')),
-      body: const Center(child: Text('This is the settings')),
+      appBar: AppBar(title: const Text(strings.settings)),
+      body: ListView.builder(
+        padding: EdgeInsets.only(bottom: context.rawSpacing(Spacing.xLarge)),
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          final item = items[index];
+
+          if (item is SettingsSectionItem) {
+            return Text(
+              strings.settingsDescriptionForSection(item.section),
+              style: Theme.of(context).textTheme.subtitle1,
+            ).withOnlyPadding(context, top: Spacing.xLarge, bottom: Spacing.xxSmall);
+          } else if (item is LinkSettingsItem) {
+            return ExternalLinkButton(
+              item.url,
+              description: item.description,
+            ).withOnlyPadding(context, top: Spacing.xSmall);
+          } else if (item is NamedLinkSettingsItem) {
+            return ExternalLinkButton(
+              item.url,
+              description: strings.settingsDescriptionForNamedLink(item.linkSettings),
+            ).withOnlyPadding(context, top: Spacing.xSmall);
+          } else if (item is NamedCustomSettingsItem) {
+            return LinkButton(
+              onTap: () {
+                switch (item.customSettings) {
+                  case NamedCustomSettings.licenses:
+                    _navigateToLicense(context, licensePageBackgroundColor: licenseBg);
+                    break;
+                }
+              },
+              text: strings.settingsDescriptionForNamedCustom(item.customSettings),
+            ).withOnlyPadding(context, top: Spacing.xSmall);
+          }
+
+          throw InconsistentStateError.layout('Unsupported subtype (${item.runtimeType}) of `SettingsItem`');
+        },
+      ).withSymmetricalPadding(context, horizontal: Spacing.medium),
+    );
+  }
+
+  void _navigateToLicense(BuildContext context, {required Color licensePageBackgroundColor}) {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (context) => Theme(
+          // cardColor is used by the LicensePage to draw its background color
+          data: Theme.of(context).copyWith(cardColor: licensePageBackgroundColor),
+          child: const LicensePage(),
+        ),
+      ),
     );
   }
 }
---
+++ lib/application/view-models/settings/settings_vm.dart
@@ -0,0 +1,88 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final settingsVM = Provider((_) => SettingsVM());
+
+class SettingsVM {
+  SettingsVM()
+      : items = _items.entries
+            .map((entry) => [SettingsSectionItem(entry.key), ...entry.value])
+            .expand((element) => element)
+            .toList();
+
+  final List<SettingsItem> items;
+}
+
+const _items = {
+  SettingsSection.legal: [
+    // TODO(matuella): Add link when available
+    NamedLinkSettingsItem(NamedLinkSettings.termsAndPrivacyPolicy, url: 'https://google.com/'),
+    NamedCustomSettingsItem(NamedCustomSettings.licenses),
+  ],
+  SettingsSection.sponsors: [
+    LinkSettingsItem(description: 'Rocketseat', url: 'https://rocketseat.com.br/'),
+    LinkSettingsItem(description: 'Startup Life', url: 'https://startuplife.com.br/'),
+    LinkSettingsItem(description: 'Pingback', url: 'https://pingback.com/about'),
+  ],
+  SettingsSection.organizers: [
+    LinkSettingsItem(description: 'Lucas Montano', url: 'https://www.youtube.com/channel/UCyHOBY6IDZF9zOKJPou2Rgg'),
+    LinkSettingsItem(description: 'Olympus', url: 'https://olmps.co/'),
+  ],
+};
+
+/// Base class to be implemented by any metadata related to a [SettingsSection]
+///
+/// The purpose of this class and its subclasses are only to make the UI work easier and more agnostic to any possible
+/// logic, being responsible solely for rendering the layout given each metadata.
+abstract class SettingsItem extends Equatable {
+  const SettingsItem();
+}
+
+/// Available settings sections
+enum SettingsSection { legal, sponsors, organizers }
+
+/// Named settings that links to external sources but have a locale-specific naming
+enum NamedLinkSettings { termsAndPrivacyPolicy }
+
+/// Named settings that have a locale-specific naming and custom navigation behavior
+enum NamedCustomSettings { licenses }
+
+class SettingsSectionItem extends SettingsItem {
+  const SettingsSectionItem(this.section);
+
+  final SettingsSection section;
+
+  @override
+  List<Object?> get props => [section];
+}
+
+abstract class SettingsContent extends SettingsItem {
+  const SettingsContent();
+}
+
+/// Generic settings item that should open an external [url]
+class LinkSettingsItem extends SettingsContent {
+  const LinkSettingsItem({required this.description, required this.url});
+  final String description;
+  final String url;
+
+  @override
+  List<Object?> get props => [description, url];
+}
+
+class NamedLinkSettingsItem extends SettingsContent {
+  const NamedLinkSettingsItem(this.linkSettings, {required this.url});
+  final NamedLinkSettings linkSettings;
+  final String url;
+
+  @override
+  List<Object?> get props => [linkSettings, url];
+}
+
+class NamedCustomSettingsItem extends SettingsContent {
+  const NamedCustomSettingsItem(this.customSettings);
+  final NamedCustomSettings customSettings;
+
+  @override
+  List<Object?> get props => [customSettings];
+}
