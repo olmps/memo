aabd5c25159009baad836aeeec862b298bd39d33
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-05-04 10:00:10 -0300

    Updates version handling:
    - adds `ResourceServices`;
    - removes `_version.json` file;
    - removes `VersionRepository.getLocalCollectionVersions`, `UserRepository.getLastCollectionsVersions` and `UserRepository.updateCollectionsVersions` in favor of adding `VersionRepository.getCurrentApplicationVersion`, `VersionRepository.getStoredApplicationVersion` and `VersionRepository.updateToLatestApplicationVersion`;
    - removes unused functions in `CollectionServices` and `UserRepository`;
    - adds `CollectionServices.listenToCollectionStatus`.

--- lib/data/repositories/version_repository.dart
+++ lib/data/repositories/version_repository.dart
@@ -1,20 +1,53 @@
-import 'package:memo/data/gateways/application_bundle.dart';
+import 'package:memo/data/gateways/sembast_database.dart';
+import 'package:package_info/package_info.dart';
 
 /// Handles all read, write and serialization operations pertaining to this application's versioning
 abstract class VersionRepository {
-  /// Retrieves a map that associated a `Collection` name as its key and its version as the value
-  Future<Map<String, int>> getLocalCollectionVersions();
+  /// Retrieves the latest application version
+  Future<String> getCurrentApplicationVersion();
+
+  /// Retrieves the last stored application version
+  ///
+  /// May return `null` if the application version was never stored before
+  Future<String?> getStoredApplicationVersion();
+
+  /// Uses the current application version to update the stored application version
+  ///
+  /// This is nothing but a helper to call [getCurrentApplicationVersion] and store its returning value.
+  Future<void> updateToLatestApplicationVersion();
 }
 
 class VersionRepositoryImpl implements VersionRepository {
-  VersionRepositoryImpl(this._appBundle);
-  final ApplicationBundle _appBundle;
-  static const _versionFile = 'assets/collections/_version.json';
+  VersionRepositoryImpl(this._db);
+
+  final SembastDatabase _db;
+  final _mainStore = '';
+  final _metadataRecord = 'application_metadata';
+  final _metadataVersionKey = 'version';
 
   @override
-  Future<Map<String, int>> getLocalCollectionVersions() async {
-    final dynamic versionsJson = await _appBundle.loadJson(_versionFile);
+  Future<String> getCurrentApplicationVersion() async {
+    final packageInfo = await PackageInfo.fromPlatform();
 
-    return Map<String, int>.from(versionsJson as Map<String, dynamic>);
+    final version = packageInfo.version;
+    final buildNumber = packageInfo.buildNumber;
+
+    return version + buildNumber;
   }
+
+  @override
+  Future<String?> getStoredApplicationVersion() async {
+    final appMetadata = await _db.get(id: _metadataRecord, store: _mainStore);
+    final dynamic appVersion = appMetadata?[_metadataVersionKey];
+    return appVersion != null ? appVersion as String : null;
+  }
+
+  @override
+  Future<void> updateToLatestApplicationVersion() async => _db.put(
+        id: _metadataRecord,
+        object: <String, String>{
+          _metadataVersionKey: await getCurrentApplicationVersion(),
+        },
+        store: _mainStore,
+      );
 }
