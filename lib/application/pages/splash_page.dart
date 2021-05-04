f9ae03c4b324b092905d88dd0d62ec905d9c651b
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-05-04 08:50:43 -0300

    Updates `SplashPage` and adds `logo` asset

--- lib/application/pages/splash_page.dart
+++ lib/application/pages/splash_page.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:memo/application/constants/images.dart' as images;
+
+const _splashBackgroundColor = Color(0xFF1F1D28);
 
 class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      backgroundColor: _splashBackgroundColor,
       body: Center(
-        child: Text('MEMO', style: TextStyle(fontSize: 72)),
+        child: FractionallySizedBox(
+          widthFactor: 0.5,
+          child: Image.asset(images.logoAsset),
+        ),
       ),
     );
   }
