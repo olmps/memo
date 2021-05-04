9bc0e43f966150f5ef852924d470e3497a827ab3
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-05-04 08:56:06 -0300

    Updates `license_update` with the correct path (was crashing when calling `LicensePage`)

--- lib/application/utils/license_update.dart
+++ lib/application/utils/license_update.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
+const _licensesRoot = 'assets/licenses';
+
 /// Updates the `LicenseRegistry` to include unspecified third party licenses
 ///
 /// While flutter include all `pubspec` related licenses in the registry by default, there are a couple that are used
 /// _indirectly_, thus needing to be added manually into the `LicenseRegistry`.
 void updateLicenseRegistry() {
   LicenseRegistry.addLicense(() async* {
-    const robotoMonoLicense = 'fonts/RobotoMono-LICENSE.txt';
+    const robotoMonoLicense = '$_licensesRoot/RobotoMono-LICENSE.txt';
     final license = await rootBundle.loadString(robotoMonoLicense);
 
     yield LicenseEntryWithLineBreaks(['RobotoMono'], license);
