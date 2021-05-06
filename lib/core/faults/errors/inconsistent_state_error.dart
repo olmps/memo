bdb04394239f08e395cba8de40083dad66d64301
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-05-06 15:39:50 -0300

    Adds `InconsistentStateError.gateway`

--- lib/core/faults/errors/inconsistent_state_error.dart
+++ lib/core/faults/errors/inconsistent_state_error.dart
@@ -14,24 +14,28 @@ class InconsistentStateError extends BaseError {
   ///   - [InconsistentStateError.service] - for inconsistent states in services;
   ///   - [InconsistentStateError.repository] - for inconsistent states in repositories;
   ///   - [InconsistentStateError.viewModel] - for inconsistent states in VMs;
-  ///   - [InconsistentStateError.layout] - for inconsistent states in the layout.
+  ///   - [InconsistentStateError.layout] - for inconsistent states in the layout;
+  ///   - [InconsistentStateError.gateway] - for inconsistent states in gateways.
   InconsistentStateError(String message) : super(type: ErrorType.inconsistentState, message: message);
 
   /// Creates a new error to represent the coordinator's inconsistent state
   InconsistentStateError.coordinator(String message)
       : super(type: ErrorType.coordinatorInconsistentState, message: message);
 
-  /// Creates a new error to represent a service's inconsistent state
+  /// Creates a new error to represent a service inconsistent state
   InconsistentStateError.service(String message) : super(type: ErrorType.serviceInconsistentState, message: message);
 
-  /// Creates a new error to represent a repository's inconsistent state
+  /// Creates a new error to represent a repository inconsistent state
   InconsistentStateError.repository(String message)
       : super(type: ErrorType.repositoryInconsistentState, message: message);
 
-  /// Creates a new error to represent a viewModel's inconsistent state
+  /// Creates a new error to represent a viewModel inconsistent state
   InconsistentStateError.viewModel(String message)
       : super(type: ErrorType.viewModelInconsistentState, message: message);
 
   /// Creates a new error to represent a layout inconsistent state
   InconsistentStateError.layout(String message) : super(type: ErrorType.layoutInconsistentState, message: message);
+
+  /// Creates a new error to represent a gateway inconsistent state
+  InconsistentStateError.gateway(String message) : super(type: ErrorType.gatewayInconsistentState, message: message);
 }
