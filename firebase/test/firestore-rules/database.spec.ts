acd1cd1b5330fa694d8212bd0d895dce150a69ad
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-08-02 10:40:06 -0300

    Adds all `firestore-rules`

---
+++ firebase/test/firestore-rules/database.spec.ts
@@ -0,0 +1,43 @@
+import * as rulesTesting from "@firebase/rules-unit-testing";
+import * as utils from "./utils";
+
+describe("databases/{database}/documents/", () => {
+  const randomCollectionId = "any-random-collection";
+  const randomDocId = "random-doc-id";
+  let randomCollection: utils.CollectionReference;
+  let firestore: utils.ClientFirestore;
+
+  describe("authenticated users", () => {
+    before(async () => {
+      firestore = await utils.createMyFirestore();
+      randomCollection = firestore.collection(randomCollectionId);
+    });
+
+    it("should be denied to read an unknown collection", async () => {
+      await rulesTesting.assertFails(randomCollection.get());
+      await rulesTesting.assertFails(randomCollection.doc(randomDocId).get());
+    });
+
+    it("should be denied to write an unknown collection", async () => {
+      await rulesTesting.assertFails(randomCollection.add({}));
+      await rulesTesting.assertFails(randomCollection.doc(randomDocId).set({}));
+    });
+  });
+
+  describe("unauthenticated users", () => {
+    before(async () => {
+      firestore = await utils.createFirestore();
+      randomCollection = firestore.collection(randomCollectionId);
+    });
+
+    it("should be denied to read an unknown collection", async () => {
+      await rulesTesting.assertFails(randomCollection.get());
+      await rulesTesting.assertFails(randomCollection.doc(randomDocId).get());
+    });
+
+    it("should be denied to write an unknown collection", async () => {
+      await rulesTesting.assertFails(randomCollection.add({}));
+      await rulesTesting.assertFails(randomCollection.doc(randomDocId).set({}));
+    });
+  });
+});
