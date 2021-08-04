baa315cab2ce1241acb777fc015feb58c686d2fc
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-08-04 11:53:55 -0300

    Removes unnecessary async calls in `test/firestore-rules`

--- firebase/test/firestore-rules/users/collections-executions/memos-executions.spec.ts
+++ firebase/test/firestore-rules/users/collections-executions/memos-executions.spec.ts
@@ -7,8 +7,8 @@ describe("users/{userId}/collections_executions/{doc}/memos_executions", () => {
   let firestore: utils.ClientFirestore;
 
   describe("owner users", () => {
-    before(async () => {
-      firestore = await utils.createMyFirestore();
+    before(() => {
+      firestore = utils.createMyFirestore();
       memosExecutionsRef = firestore.collection(collectionId(utils.myFirestoreUid));
     });
 
