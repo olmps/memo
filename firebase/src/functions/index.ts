97225cf8f4c218c49e5644e84a00bbc9acce0911
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-09-27 10:11:17 -0300

    Update functions to use `tsconfig-paths` properly

--- firebase/src/functions/index.ts
+++ firebase/src/functions/index.ts
@@ -1,3 +1,7 @@
+import { registerTSPaths } from "./ts-paths";
+// Always make sure that this is called before any other import/declaration.
+registerTSPaths();
+
 import * as functions from "firebase-functions";
 import { dummy } from "../core/dummy";
 
