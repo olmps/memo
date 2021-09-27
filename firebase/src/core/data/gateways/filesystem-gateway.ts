9c90138b4454d58380171b1902fca95e14003549
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-09-27 10:41:01 -0300

    Add first `FileSystemGateway` proposal

---
+++ firebase/src/core/data/gateways/filesystem-gateway.ts
@@ -0,0 +1,38 @@
+import * as fs from "fs";
+import FilesystemError from "@faults/errors/filesystem-error";
+
+/** Exposes common filesystem operations. */
+export class FileSystemGateway {
+  readonly #root = process.cwd();
+
+  /**
+   * Read all files in {@link dir}.
+   *
+   * @reject {FilesystemError} Something went wrong while reading the filesystem.
+   * @fulfill {string[]} List of all files as raw strings.
+   */
+  async readDirFilesAsStrings(
+    dir: string,
+    {
+      encoding,
+    }: {
+      encoding: BufferEncoding;
+    } = { encoding: "utf-8" }
+  ): Promise<string[]> {
+    const processDir = `${this.#root}/${dir}`;
+    try {
+      const files = await fs.promises.readdir(processDir);
+      const result = await Promise.all(
+        files.map((file) => {
+          return fs.promises.readFile(`${processDir}/${file}`, { encoding });
+        })
+      );
+
+      return result;
+    } catch (err) {
+      return Promise.reject(
+        new FilesystemError({ message: `Failed to read files in directory "${processDir}".`, origin: err })
+      );
+    }
+  }
+}
