372938fdc553fd67cc4c93b1bc4aecff12b380fc
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-09-27 10:59:44 -0300

    Add `FirestoreGateway` proposal

--- firebase/src/core/faults/errors/firebase-firestore-error.ts
+++ firebase/src/core/faults/errors/firebase-firestore-error.ts
@@ -1,6 +1,6 @@
 import { BaseError } from "../fault";
 
-export class FirebaseFirestoreError extends BaseError {
+export default class FirebaseFirestoreError extends BaseError {
   constructor(fault: { message: string; origin?: unknown }) {
     super({ type: "firebase/firestore", ...fault });
   }
