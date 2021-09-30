e5b3456af1a2ad4b349a79bd0a5d8b49f91ab5d1
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-09-30 11:52:48 -0300

    Revert "Add `FirestoreGateway` proposal"
    This reverts commit 03a3950ea64c6f4e1b8ad90f53493eebe291f02e.

--- firebase/src/core/faults/errors/firebase-firestore-error.ts
+++ firebase/src/core/faults/errors/firebase-firestore-error.ts
@@ -1,6 +1,6 @@
 import { BaseError } from "../fault";
 
-export default class FirebaseFirestoreError extends BaseError {
+export class FirebaseFirestoreError extends BaseError {
   constructor(fault: { message: string; origin?: unknown }) {
     super({ type: "firebase/firestore", ...fault });
   }
