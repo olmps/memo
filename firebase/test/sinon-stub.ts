e269c639f48b93de161dfbadd0bc88d078471839
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-09-30 11:52:48 -0300

    Revert "Add `createSinonStub` testing utility"
    This reverts commit ca9b45ccb2351e557104717d6e78228d09054039.

--- firebase/test/sinon-stub.ts
+++
@@ -1,8 +0,0 @@
-import * as sinon from "sinon";
-
-type StubbedClass<T> = sinon.SinonStubbedInstance<T> & T;
-
-/** Creates a type-casted sinon Stub. */
-export default function createSinonStub<T>(constructor: sinon.StubbableType<T>): StubbedClass<T> {
-  return sinon.createStubInstance(constructor) as StubbedClass<T>;
-}
