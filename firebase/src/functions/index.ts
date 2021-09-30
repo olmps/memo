7fa408a0fc12496dddf4b98570220309db609dd4
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-09-30 11:52:48 -0300

    Revert "Adds dummy example files for functions and scripts"
    This reverts commit 1d2b6dcd2742cb80fff80ebbc766424dd45eb8ab.

--- firebase/src/functions/index.ts
+++
@@ -1,6 +0,0 @@
-import * as functions from "firebase-functions";
-import { dummy } from "../core/dummy";
-
-export const dummyFunction = functions.https.onRequest((_, res) => {
-  res.status(200).send(dummy.value);
-});
--- firebase/src/scripts/dummy-script.ts
+++
@@ -1,4 +0,0 @@
-import { dummy } from "../core/dummy";
-
-export const scriptValue = 1;
-console.log(dummy.value);
