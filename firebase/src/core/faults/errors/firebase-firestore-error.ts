fefb45468fdff9b0707adad0e188c4c0235b5166
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-09-30 11:52:48 -0300

    Revert "Add `FirebaseFirestoreError`"
    This reverts commit 279cb74265e2f04b6283de5f9d0204b65320695c.

--- firebase/src/core/faults/errors/firebase-firestore-error.ts
+++
@@ -1,7 +0,0 @@
-import { BaseError } from "../fault";
-
-export class FirebaseFirestoreError extends BaseError {
-  constructor(fault: { message: string; origin?: unknown }) {
-    super({ type: "firebase/firestore", ...fault });
-  }
-}
