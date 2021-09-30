0dbb9b461eaaceb9d0fb763a93a8f76d2cd65560
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-09-30 11:52:48 -0300

    Revert "Update typed firestore paths"
    This reverts commit a1b980ee5374adfef0f489c41d8224e6eac6b35a.

--- firebase/src/core/data/gateways/firestore-collection-name.ts
+++ firebase/src/core/data/gateways/firestore-collection-name.ts
@@ -1,19 +1,22 @@
-/** Typed firestore collections paths. */
-export type FirestorePaths = PublicPaths | UsersPaths;
-
-/** Raw string that represent any collection's document id. */
-type Id = string;
+/** Typed firestore collections. */
+export type FirestoreCollectionName =
+  | PublicCollectionName
+  | PublicSubCollectionName
+  | UserCollectionName
+  | UserSubCollectionName;
 
 /** Public collections available to all authenticated users. */
-type PublicPaths = PublicCollectionsPaths | "collection_categories";
+type PublicCollectionName = "public_collections" | "public_collection_categories" | "public_memos";
 
-/** Collection-related paths (the entity, not the Firestore one) paths, available to all authenticated users. */
-type PublicCollectionsPaths = "collections" | `collections/${Id}/memos`;
+/** Public subcollections available to all authenticated users. */
+type PublicSubCollectionName = `public_collections/${string}/public_memos`;
 
-/** User-related collection paths, which are available to the respective authenticated users. */
-type UsersPaths =
-  | `users/${Id}/collections`
-  | `users/${Id}/collections/${Id}/memos`
-  | `users/${Id}/collection_categories`
-  | `users/${Id}/collection_executions`
-  | `users/${Id}/collection_executions/${Id}/memo_executions`;
+type UserCollectionName = "users";
+
+/** User-owned subcollections available to all respective authenticated users. */
+type UserSubCollectionName =
+  | `${UserCollectionName}/${string}/collections`
+  | `${UserCollectionName}/${string}/collections/${string}/memos`
+  | `${UserCollectionName}/${string}/collection_categories`
+  | `${UserCollectionName}/${string}/collection_executions`
+  | `${UserCollectionName}/${string}/collection_executions/${string}/memo_executions`;
