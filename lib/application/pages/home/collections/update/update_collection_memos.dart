0abeff8a08748eb10b722789c545390f0c6bf401
Guilherme Girotto <guiga741@gmail.com>
2021-11-04 18:02:22 -0300

    Adds details and memos pages

---
+++ lib/application/pages/home/collections/update/update_collection_memos.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class UpdateCollectionMemos extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.blue, child: Center(child: Text('Memos')));
+  }
+}
