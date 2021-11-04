37fb376a6bd4b88a37feae5015edf3ea43e259b2
Guilherme Girotto <guiga741@gmail.com>
2021-11-04 15:53:23 -0300

    Adds editor toolbar items assets

--- lib/application/constants/images.dart
+++ lib/application/constants/images.dart
@@ -9,14 +9,18 @@ const _imgsRoot = 'assets/images';
 @visibleForTesting
 enum ImageKey {
   // icons
+  bold,
   chevronLeft,
   chevronRight,
+  code,
   close,
   clear,
   folder,
+  italic,
   link,
   settings,
   trendingUpArrow,
+  underline,
 
   // illustrations
   easyEmoji,
@@ -31,6 +35,10 @@ extension ImageKeyPath on ImageKey {
   @visibleForTesting
   String get path {
     switch (this) {
+      case ImageKey.bold:
+        return '$_iconsRoot/bold.png';
+      case ImageKey.code:
+        return '$_iconsRoot/code.png';
       case ImageKey.chevronLeft:
         return '$_iconsRoot/chevron_left.png';
       case ImageKey.chevronRight:
@@ -41,12 +49,16 @@ extension ImageKeyPath on ImageKey {
         return '$_iconsRoot/clear.png';
       case ImageKey.folder:
         return '$_iconsRoot/folder.png';
+      case ImageKey.italic:
+        return '$_iconsRoot/italic.png';
       case ImageKey.link:
         return '$_iconsRoot/link.png';
       case ImageKey.settings:
         return '$_iconsRoot/settings.png';
       case ImageKey.trendingUpArrow:
         return '$_iconsRoot/trending_up_arrow.png';
+      case ImageKey.underline:
+        return '$_iconsRoot/underline.png';
       case ImageKey.easyEmoji:
         return '$_illustrationsRoot/easy_emoji.png';
       case ImageKey.folderBig:
@@ -64,14 +76,18 @@ extension ImageKeyPath on ImageKey {
 }
 
 const _iconsRoot = '$_imgsRoot/icons';
+final bold = ImageKey.bold.path;
 final chevronLeftAsset = ImageKey.chevronLeft.path;
 final chevronRightAsset = ImageKey.chevronRight.path;
+final code = ImageKey.code.path;
 final closeAsset = ImageKey.close.path;
 final clearAsset = ImageKey.clear.path;
 final folderAsset = ImageKey.folder.path;
+final italic = ImageKey.italic.path;
 final linkAsset = ImageKey.link.path;
 final settingsAsset = ImageKey.settings.path;
 final trendingUpArrowAsset = ImageKey.trendingUpArrow.path;
+final underline = ImageKey.underline.path;
 
 const _illustrationsRoot = '$_imgsRoot/illustrations';
 final easyEmojiAsset = ImageKey.easyEmoji.path;
