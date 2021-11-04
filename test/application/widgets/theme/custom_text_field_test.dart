849f0c0718c62675f1bdcfb8b270a41b385584b1
Guilherme Girotto <guiga741@gmail.com>
2021-11-04 13:52:39 -0300

    Improves `CustomTextField` tests description and adds more use-cases

--- test/application/widgets/theme/custom_text_field_test.dart
+++ test/application/widgets/theme/custom_text_field_test.dart
@@ -7,7 +7,7 @@ import '../../../utils/widget_pump.dart';
 
 void main() {
   group('Editing State - ', () {
-    testWidgets('should have the expected text in the controller after typing in the TextField', (tester) async {
+    testWidgets('should have the expected text in the controller after typing', (tester) async {
       final controller = TextEditingController();
       final textField = CustomTextField(controller: controller);
       const expectedText = 'any';
@@ -18,7 +18,7 @@ void main() {
       expect(controller.text, expectedText);
     });
 
-    testWidgets('should have focus when tapping in the TextField', (tester) async {
+    testWidgets('should have focus when tapped', (tester) async {
       final focusNode = FocusNode();
       final textField = CustomTextField(focusNode: focusNode);
 
@@ -43,7 +43,9 @@ void main() {
       renderedTextField = find.byType(TextField).first.evaluate().single.widget as TextField;
       expect(renderedTextField.decoration!.suffixIcon, isA<IconButton>());
     });
+  });
 
+  group('Suffix Icon', () {
     testWidgets('should show custom suffix icon when not focused', (tester) async {
       final suffixIcon = Container(color: Colors.red);
       final textField = CustomTextField(suffixIcon: suffixIcon);
@@ -54,7 +56,29 @@ void main() {
       expect(renderedTextField.decoration!.suffixIcon, suffixIcon);
     });
 
-    testWidgets('should replace custom suffix icon by clear button when editing', (tester) async {
+    testWidgets('should show custom suffix icon when focused but with empty text', (tester) async {
+      final suffixIcon = Container(color: Colors.red);
+      final textField = CustomTextField(suffixIcon: suffixIcon);
+
+      await pumpMaterialScopedWithTheme(tester, textField);
+      await tester.enterText(find.byType(CustomTextField), '');
+
+      final renderedTextField = find.byType(TextField).first.evaluate().single.widget as TextField;
+      expect(renderedTextField.decoration!.suffixIcon, suffixIcon);
+    });
+
+    testWidgets('should show custom suffix icon when editing and showClearIcon is false', (tester) async {
+      final suffixIcon = Container(color: Colors.red);
+      final textField = CustomTextField(suffixIcon: suffixIcon, showsClearIcon: false);
+
+      await pumpMaterialScopedWithTheme(tester, textField);
+      await tester.enterText(find.byType(CustomTextField), 'any');
+
+      final renderedTextField = find.byType(TextField).first.evaluate().single.widget as TextField;
+      expect(renderedTextField.decoration!.suffixIcon, suffixIcon);
+    });
+
+    testWidgets('should show clear button when has text, is focused and showsClearIcon is true', (tester) async {
       final suffixIcon = Container(color: Colors.red);
       final textField = CustomTextField(suffixIcon: suffixIcon);
 
@@ -70,32 +94,60 @@ void main() {
     });
   });
 
-  group('Error State - ', () {
-    testWidgets('should present error label when errorText is not null', (tester) async {
-      const errorText = 'any';
-      const textField = CustomTextField(errorText: errorText);
+  group('Labels - ', () {
+    group('Error Label - ', () {
+      testWidgets('should present error label when errorText is not null', (tester) async {
+        const errorText = 'any';
+        const textField = CustomTextField(errorText: errorText);
 
-      await pumpMaterialScopedWithTheme(tester, textField);
+        await pumpMaterialScopedWithTheme(tester, textField);
 
-      final errorTextWidget = find.byType(Text).last.evaluate().single.widget as Text;
-      expect(errorTextWidget.data, errorText);
+        final errorTextWidget = find.text(errorText);
+        expect(errorTextWidget, findsOneWidget);
+      });
+
+      testWidgets('should have a destructive border when errorText is not null', (tester) async {
+        const errorText = 'any';
+        const textField = CustomTextField(errorText: errorText);
+        final expectedColor = ThemeController().state.destructiveSwatch;
+
+        await pumpMaterialScopedWithTheme(tester, textField);
+
+        final wrapperContainer = find.byType(Container).first.evaluate().single.widget as Container;
+        final containerDecoration = wrapperContainer.decoration! as BoxDecoration;
+        final containerBorder = containerDecoration.border! as Border;
+
+        expect(containerBorder.top.color, expectedColor);
+        expect(containerBorder.right.color, expectedColor);
+        expect(containerBorder.bottom.color, expectedColor);
+        expect(containerBorder.left.color, expectedColor);
+      });
+
+      testWidgets('should precede helperText when both are present', (tester) async {
+        const errorText = 'error text';
+        const helperText = 'helper text';
+        const textField = CustomTextField(errorText: errorText, helperText: helperText);
+
+        await pumpMaterialScopedWithTheme(tester, textField);
+
+        final errorTextWidget = find.text(errorText);
+        final helperTextWidget = find.text(helperText);
+
+        expect(errorTextWidget, findsOneWidget);
+        expect(helperTextWidget, findsNothing);
+      });
     });
 
-    testWidgets('should have a destructive border when errorText is not null', (tester) async {
-      const errorText = 'any';
-      const textField = CustomTextField(errorText: errorText);
-      final expectedColor = ThemeController().state.destructiveSwatch;
+    group('Helper Label', () {
+      testWidgets('should present helper label when helperText is not null', (tester) async {
+        const helperText = 'any';
+        const textField = CustomTextField(helperText: helperText);
 
-      await pumpMaterialScopedWithTheme(tester, textField);
+        await pumpMaterialScopedWithTheme(tester, textField);
 
-      final wrapperContainer = find.byType(Container).first.evaluate().single.widget as Container;
-      final containerDecoration = wrapperContainer.decoration! as BoxDecoration;
-      final containerBorder = containerDecoration.border! as Border;
-
-      expect(containerBorder.top.color, expectedColor);
-      expect(containerBorder.right.color, expectedColor);
-      expect(containerBorder.bottom.color, expectedColor);
-      expect(containerBorder.left.color, expectedColor);
+        final helperTextWidget = find.text(helperText);
+        expect(helperTextWidget, findsOneWidget);
+      });
     });
   });
 }
