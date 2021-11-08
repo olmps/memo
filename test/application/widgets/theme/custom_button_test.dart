b0ca192433145904192dc8d68b12709df51cd214
Guilherme Girotto <guiga741@gmail.com>
2021-11-08 09:21:04 -0300

    Updates tests

--- test/application/widgets/theme/custom_button_test.dart
+++ test/application/widgets/theme/custom_button_test.dart
@@ -1,56 +1,205 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:memo/application/constants/images.dart' as images;
 import 'package:memo/application/widgets/theme/custom_button.dart';
 
 import '../../../utils/widget_pump.dart';
 
 void main() {
-  group('Custom Buttons -', () {
-    testWidgets('should init with `normal` state when `onPressed` is not null', (tester) async {
-      final primaryButton = PrimaryElevatedButton(text: 'test', onPressed: () {});
+  group('PrimaryElevatedButton -', () {
+    const buttonColor = Colors.red;
+    final normalColor = buttonColor.shade400;
+    const pressedColor = buttonColor;
+    final disabledColor = buttonColor.withOpacity(0.4);
+
+    testWidgets('should inits with normal background color when onPressed is not null', (tester) async {
+      final primaryButton = PrimaryElevatedButton(text: 'test', onPressed: () {}, backgroundColor: buttonColor);
 
       await pumpMaterialScopedWithTheme(tester, primaryButton);
 
-      final buttonState = tester.state<CustomButtonState>(find.byType(CustomButton));
-
-      expect(buttonState.state == ButtonState.normal, true);
+      final buttonContainer = tester.widget(find.byType(Container)) as Container;
+      final buttonDecoration = buttonContainer.decoration! as BoxDecoration;
+      expect(buttonDecoration.color, normalColor);
     });
 
-    testWidgets('should stay in `disable` state when `onPressed` is `null`', (tester) async {
-      const primaryButton = PrimaryElevatedButton(text: 'test');
+    testWidgets('should update its background color when disabled', (tester) async {
+      const primaryButton = PrimaryElevatedButton(text: 'test', backgroundColor: buttonColor);
 
       await pumpMaterialScopedWithTheme(tester, primaryButton);
 
-      final buttonState = tester.state<CustomButtonState>(find.byType(CustomButton));
-
-      expect(buttonState.state == ButtonState.disabled, true);
+      final buttonContainer = tester.widget(find.byType(Container)) as Container;
+      final buttonDecoration = buttonContainer.decoration! as BoxDecoration;
+      expect(buttonDecoration.color, disabledColor);
     });
 
-    testWidgets('should update to `pressed` state when tapped and `onPressed` is not `null`', (tester) async {
-      final primaryButton = PrimaryElevatedButton(text: 'test', onPressed: () {});
+    testWidgets('should update its background color when pressed', (tester) async {
+      final primaryButton = PrimaryElevatedButton(text: 'test', onPressed: () {}, backgroundColor: buttonColor);
 
       await pumpMaterialScopedWithTheme(tester, primaryButton);
-
       await tester.startGesture(const Offset(0, 0));
-
       await tester.pump();
 
-      final buttonState = tester.state<CustomButtonState>(find.byType(CustomButton));
-
-      expect(buttonState.state == ButtonState.pressed, true);
+      final buttonContainer = tester.widget(find.byType(Container)) as Container;
+      final buttonDecoration = buttonContainer.decoration! as BoxDecoration;
+      expect(buttonDecoration.color, pressedColor);
     });
 
-    testWidgets("shouldn't change to `pressed` when tapped and `onPressed` is `null`", (tester) async {
-      const primaryButton = PrimaryElevatedButton(text: 'test');
+    testWidgets('should not update its background color when pressed without onPressed', (tester) async {
+      const primaryButton = PrimaryElevatedButton(text: 'test', backgroundColor: buttonColor);
 
       await pumpMaterialScopedWithTheme(tester, primaryButton);
-
       await tester.startGesture(const Offset(0, 0));
-
       await tester.pump();
 
-      final buttonState = tester.state<CustomButtonState>(find.byType(CustomButton));
+      final buttonContainer = tester.widget(find.byType(Container)) as Container;
+      final buttonDecoration = buttonContainer.decoration! as BoxDecoration;
+      expect(buttonDecoration.color, disabledColor);
+    });
 
-      expect(buttonState.state == ButtonState.disabled, true);
+    testWidgets('should trigger onPressed when tapped', (tester) async {
+      var hasPressed = false;
+      final primaryButton = PrimaryElevatedButton(text: 'test', onPressed: () => hasPressed = true);
+
+      await pumpMaterialScopedWithTheme(tester, primaryButton);
+      await tester.tap(find.byType(PrimaryElevatedButton));
+      await tester.pump();
+
+      expect(hasPressed, true);
+    });
+  });
+
+  group('SecondaryElevatedButton -', () {
+    const buttonColor = Colors.red;
+    final normalColor = buttonColor.shade700;
+    final pressedColor = buttonColor.shade800;
+    final disabledColor = buttonColor.shade800.withOpacity(0.4);
+
+    testWidgets('should inits with normal background color when onPressed is not null', (tester) async {
+      final secondaryButton = SecondaryElevatedButton(text: 'test', onPressed: () {}, backgroundColor: buttonColor);
+
+      await pumpMaterialScopedWithTheme(tester, secondaryButton);
+
+      final buttonContainer = tester.widget(find.byType(Container)) as Container;
+      final buttonDecoration = buttonContainer.decoration! as BoxDecoration;
+      expect(buttonDecoration.color, normalColor);
+    });
+
+    testWidgets('should update its background color when disabled', (tester) async {
+      const secondaryButton = SecondaryElevatedButton(text: 'test', backgroundColor: buttonColor);
+
+      await pumpMaterialScopedWithTheme(tester, secondaryButton);
+
+      final buttonContainer = tester.widget(find.byType(Container)) as Container;
+      final buttonDecoration = buttonContainer.decoration! as BoxDecoration;
+      expect(buttonDecoration.color, disabledColor);
+    });
+
+    testWidgets('should update its background color when pressed', (tester) async {
+      final secondaryButton = SecondaryElevatedButton(text: 'test', onPressed: () {}, backgroundColor: buttonColor);
+
+      await pumpMaterialScopedWithTheme(tester, secondaryButton);
+      await tester.startGesture(const Offset(0, 0));
+      await tester.pump();
+
+      final buttonContainer = tester.widget(find.byType(Container)) as Container;
+      final buttonDecoration = buttonContainer.decoration! as BoxDecoration;
+      expect(buttonDecoration.color, pressedColor);
+    });
+
+    testWidgets('should not update its background color when pressed without onPressed', (tester) async {
+      const secondaryButton = SecondaryElevatedButton(text: 'test', backgroundColor: buttonColor);
+
+      await pumpMaterialScopedWithTheme(tester, secondaryButton);
+      await tester.startGesture(const Offset(0, 0));
+      await tester.pump();
+
+      final buttonContainer = tester.widget(find.byType(Container)) as Container;
+      final buttonDecoration = buttonContainer.decoration! as BoxDecoration;
+      expect(buttonDecoration.color, disabledColor);
+    });
+
+    testWidgets('should trigger onPressed when tapped', (tester) async {
+      var hasPressed = false;
+      final secondaryButton = SecondaryElevatedButton(text: 'test', onPressed: () => hasPressed = true);
+
+      await pumpMaterialScopedWithTheme(tester, secondaryButton);
+      await tester.tap(find.byType(SecondaryElevatedButton));
+      await tester.pump();
+
+      expect(hasPressed, true);
+    });
+  });
+
+  group('CustomTextButton -', () {
+    const buttonColor = Colors.red;
+    final normalColor = buttonColor.shade300;
+    final pressedColor = buttonColor.shade400;
+    final disabledColor = buttonColor.shade400.withOpacity(0.4);
+
+    testWidgets('should inits with normal text color when onPressed is not null', (tester) async {
+      final textButton = CustomTextButton(text: 'test', onPressed: () {}, color: buttonColor);
+
+      await pumpMaterialScopedWithTheme(tester, textButton);
+
+      final text = tester.widget(find.byType(Text)) as Text;
+      expect(text.style!.color, normalColor);
+    });
+
+    testWidgets('should update its text color when disabled', (tester) async {
+      const textButton = CustomTextButton(text: 'test', color: buttonColor);
+
+      await pumpMaterialScopedWithTheme(tester, textButton);
+
+      final text = tester.widget(find.byType(Text)) as Text;
+      expect(text.style!.color, disabledColor);
+    });
+
+    testWidgets('should update its text color when pressed', (tester) async {
+      final textButton = CustomTextButton(text: 'test', onPressed: () {}, color: buttonColor);
+
+      await pumpMaterialScopedWithTheme(tester, textButton);
+      await tester.startGesture(const Offset(0, 0));
+      await tester.pump();
+
+      final text = tester.widget(find.byType(Text)) as Text;
+      expect(text.style!.color, pressedColor);
+    });
+
+    testWidgets('should not update its text color when pressed without onPressed', (tester) async {
+      const textButton = CustomTextButton(text: 'test', color: buttonColor);
+
+      await pumpMaterialScopedWithTheme(tester, textButton);
+      await tester.startGesture(const Offset(0, 0));
+      await tester.pump();
+
+      final text = tester.widget(find.byType(Text)) as Text;
+      expect(text.style!.color, disabledColor);
+    });
+
+    testWidgets('should update its leading asset color to be the same as the button', (tester) async {
+      final textButton = CustomTextButton(
+        text: 'test',
+        onPressed: () {},
+        color: buttonColor,
+        leadingAsset: images.chevronLeftAsset,
+      );
+
+      await pumpMaterialScopedWithTheme(tester, textButton);
+      await tester.pump();
+
+      final buttonLeadingAsset = tester.widget(find.byType(Image)) as Image;
+      expect(buttonLeadingAsset.color, normalColor);
+    });
+
+    testWidgets('should trigger onPressed when tapped', (tester) async {
+      var hasPressed = false;
+      final textButton = CustomTextButton(text: 'test', onPressed: () => hasPressed = true);
+
+      await pumpMaterialScopedWithTheme(tester, textButton);
+      await tester.tap(find.byType(CustomTextButton));
+      await tester.pump();
+
+      expect(hasPressed, true);
     });
   });
 }
