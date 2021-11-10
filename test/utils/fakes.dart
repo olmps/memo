d5901bcc8e2976479fba0ad1cb8b261eb2f844ef
Guilherme Girotto <guiga741@gmail.com>
2021-11-10 15:54:37 -0300

    Renames `retry_container` to `exception_retry_container` and adds tests

--- test/utils/fakes.dart
+++ test/utils/fakes.dart
@@ -1,6 +1,12 @@
+import 'package:mocktail/mocktail.dart';
+
 final question = [
   {'question': '?'}
 ];
 final answer = [
   {'answer': '!'}
 ];
+
+class MockCallbackFunction extends Mock {
+  void call();
+}
