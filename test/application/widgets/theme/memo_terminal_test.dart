6a6a3a0841132f907acb1fd9883eddf8cc7c8968
Guilherme Girotto <guiga741@gmail.com>
2021-11-12 17:57:41 -0300

    Adds `MemoTerminal` widget and its tests - closes #219

---
+++ test/application/widgets/theme/memo_terminal_test.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:memo/application/constants/strings.dart' as strings;
+import 'package:memo/application/widgets/theme/custom_button.dart';
+import 'package:memo/application/widgets/theme/destructive_button.dart';
+import 'package:memo/application/widgets/theme/memo_terminal.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../../utils/fakes.dart';
+import '../../../utils/widget_pump.dart';
+
+void main() {
+  testWidgets('should become scrollable when the content exceeds the available height', (tester) async {
+    const hugeFakeText = r'A\nHuge\nFake\nText\nThat\nWill\nExpand\nThe\nRich\nText\nEditor';
+    const encodedText = '[{"insert":"$hugeFakeText\\n"}]';
+    final scrollController = ScrollController();
+    final questionController = TextEditingController(text: encodedText);
+    final answerController = TextEditingController(text: encodedText);
+    // Wraps `MemoTerminal` in a tiny SizedBox to force its content to be scrollable
+    final memoTerminal = SizedBox.square(
+      dimension: 320,
+      child: MemoTerminal(
+        memoIndex: 0,
+        questionController: questionController,
+        answerController: answerController,
+        scrollController: scrollController,
+      ),
+    );
+    const dragOffset = 500.0;
+
+    await pumpProviderScoped(tester, memoTerminal);
+    await tester.drag(find.byType(MemoTerminal), const Offset(0, -dragOffset));
+    await tester.pump();
+
+    expect(scrollController.offset, greaterThan(0));
+  });
+
+  testWidgets('should present confirmation modal when the remove button is tapped', (tester) async {
+    final onRemoveCallback = MockCallbackFunction();
+    final memoTerminal = MemoTerminal(memoIndex: 0, onRemove: onRemoveCallback);
+
+    await pumpProviderScoped(tester, memoTerminal);
+
+    await tester.tap(find.byType(CustomTextButton));
+    await tester.pumpAndSettle();
+
+    final confirmationTitle = find.text(strings.removeMemoTitle);
+    expect(confirmationTitle, findsOneWidget);
+  });
+
+  testWidgets('should trigger onRemove when confirming memo removal', (tester) async {
+    final onRemoveCallback = MockCallbackFunction();
+    final memoTerminal = MemoTerminal(memoIndex: 0, onRemove: onRemoveCallback);
+
+    await pumpProviderScoped(tester, memoTerminal);
+
+    await tester.tap(find.byType(CustomTextButton));
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.byType(DestructiveButton));
+    await tester.pumpAndSettle();
+
+    verify(onRemoveCallback.call).called(1);
+  });
+}
