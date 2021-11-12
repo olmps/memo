31b7770c04904d672a1c804cd1b81418dc753e05
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-11-12 10:58:51 -0300

    Add to-be-implemented integrity check test on collections

---
+++ firebase/test/collections/integrity-check.spec.ts
@@ -0,0 +1,6 @@
+describe("Collections Integrity Check", () => {
+  // TODO(matuella):
+  // TEST 1: Maintain naming + id consistency
+  // TEST 2: No duplicate memos ids amongst each collection
+  // TEST 3 (?): should we check for "data consistency", such as at least 1 contributor, req. description, req. locale, etc.
+});
