8843d5ae5cc7de17554f7ae48ec0f731f0dcd58e
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-11-12 11:00:45 -0300

    Add `Memo` model and validation

---
+++ firebase/src/core/domain/models/memo.ts
@@ -0,0 +1,18 @@
+import { defaultMaxStringLength, validate } from "@utils/validate";
+import * as Joi from "joi";
+
+export interface Memo {
+  readonly id: string;
+  readonly question: Record<string, unknown>[];
+  readonly answer: Record<string, unknown>[];
+}
+
+export const memoValidationSchema = Joi.object({
+  id: Joi.string().max(defaultMaxStringLength).required(),
+  question: Joi.array().items(Joi.object()).min(1).required(),
+  answer: Joi.array().items(Joi.object()).min(1).required(),
+});
+
+export function validateMemo(memo: Memo): void {
+  validate(memoValidationSchema, memo);
+}
