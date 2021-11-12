b96026dabf0e09509a7790665d4ff632a735889e
Guilherme Girotto <guiga741@gmail.com>
2021-11-12 17:53:50 -0300

    Moves `TerminalWindow` to a dedicated file to be reused as a component (and adds its tests)

---
+++ lib/application/widgets/theme/terminal_window.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:layoutr/common_layout.dart';
+import 'package:memo/application/constants/dimensions.dart' as dimens;
+import 'package:memo/application/theme/theme_controller.dart';
+
+/// A container that decorates [body] with a terminal style.
+class TerminalWindow extends HookWidget {
+  const TerminalWindow({required this.body, required this.borderColor, required this.fadeGradient});
+
+  final Widget body;
+  final Color borderColor;
+
+  /// Fade gradient used to dim [body] content in vertical axis when it's scrollable.
+  final List<Color> fadeGradient;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      clipBehavior: Clip.hardEdge,
+      decoration: BoxDecoration(
+        color: Colors.transparent,
+        borderRadius: dimens.executionsTerminalBorderRadius,
+        border: Border.all(
+          color: borderColor,
+          width: dimens.executionsTerminalBorderWidth,
+        ),
+      ),
+      child: Stack(
+        children: [
+          Positioned.fill(child: ClipRRect(child: body)),
+          Positioned(left: 0, top: 0, right: 0, child: _TerminalHeader(fadeGradient: fadeGradient)),
+        ],
+      ),
+    );
+  }
+}
+
+class _TerminalHeader extends HookWidget {
+  const _TerminalHeader({required this.fadeGradient});
+
+  final List<Color> fadeGradient;
+  static const _actionsAmount = 3;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = useTheme();
+
+    final pseudoActions = List.generate(
+      _actionsAmount,
+      (index) => Container(
+        decoration: BoxDecoration(shape: BoxShape.circle, color: theme.neutralSwatch.shade700),
+        height: dimens.executionsTerminalActionDiameter,
+        width: dimens.executionsTerminalActionDiameter,
+      ),
+    );
+
+    return Container(
+      height: dimens.terminalWindowHeaderHeight,
+      decoration: BoxDecoration(
+        borderRadius: dimens.executionsTerminalBorderRadius,
+        gradient: LinearGradient(begin: Alignment.topCenter, end: Alignment.bottomCenter, colors: fadeGradient),
+      ),
+      child: Row(
+        children: [
+          for (final pseudoAction in pseudoActions) ...[
+            pseudoAction,
+            context.horizontalBox(Spacing.xxSmall),
+          ]
+        ],
+      ).withOnlyPadding(context, left: Spacing.medium),
+    );
+  }
+}
