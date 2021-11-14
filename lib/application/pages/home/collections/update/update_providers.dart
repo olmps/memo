ffc09a0951290671f47655b1eab5179e98cbe7b6
Guilherme Girotto <guiga741@gmail.com>
2021-11-14 11:19:59 -0300

    Updates codebase to conform with Riverpod upgrade

--- lib/application/pages/home/collections/update/update_providers.dart
+++ lib/application/pages/home/collections/update/update_providers.dart
@@ -1,19 +1,18 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:memo/application/view-models/home/update_collection_vm.dart';
 
 /// Overridable collection id used in the scope of a collection update.
-final updateCollectionId = ScopedProvider<String?>(null);
+final updateCollectionId = Provider<String?>((_) => throw UnimplementedError(), name: 'updateCollectionId');
 
-/// Syntax sugar for calling [useProvider] with both [updateCollectionVM] and [updateCollectionId] providers.
-UpdateCollectionState useUpdateCollectionState() {
-  final collectionId = useProvider(updateCollectionId);
-  return useProvider(updateCollectionVM(collectionId).state);
+/// Syntax sugar for listening to [UpdateCollectionState] state updates.
+UpdateCollectionState useUpdateCollectionState(WidgetRef ref) {
+  final collectionId = ref.watch(updateCollectionId);
+  return ref.watch(updateCollectionVM(collectionId));
 }
 
-/// Syntax sugar for `context.read` the current [UpdateCollectionVM] provider.
-UpdateCollectionVM useUpdateCollectionVM(BuildContext context) {
-  final collectionId = useProvider(updateCollectionId);
-  return context.read(updateCollectionVM(collectionId));
+/// Syntax sugar for `ref.watch` the current [UpdateCollectionVM] provider.
+UpdateCollectionVM useUpdateCollectionVM(WidgetRef ref) {
+  final collectionId = ref.watch(updateCollectionId);
+  return ref.watch(updateCollectionVM(collectionId).notifier);
 }
