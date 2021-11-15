224bf175cacbc1b6644757c3b6a41aa6a3ce2168
Guilherme Girotto <guiga741@gmail.com>
2021-11-15 17:21:08 -0300

    Removes suggestions from Tags component and updates tests

--- lib/application/constants/animations.dart
+++ lib/application/constants/animations.dart
@@ -6,3 +6,4 @@ const defaultAnimatableProgressDuration = Duration(seconds: 1);
 const terminalAnimationDuration = Duration(milliseconds: 300);
 
 const imageFadeDuration = Duration(seconds: 1);
+const textFieldHelperTextDuration = Duration(milliseconds: 150);
