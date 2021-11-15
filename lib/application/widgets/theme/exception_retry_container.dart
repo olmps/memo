da2dff86def3fea8be45a93695a28fa4e8ea7ff2
Guilherme Girotto <guiga741@gmail.com>
2021-11-15 17:37:19 -0300

    Updates docs

--- lib/application/widgets/theme/exception_retry_container.dart
+++ lib/application/widgets/theme/exception_retry_container.dart
@@ -5,10 +5,9 @@ import 'package:memo/application/constants/strings.dart' as strings;
 import 'package:memo/application/widgets/theme/custom_button.dart';
 import 'package:memo/core/faults/exceptions/base_exception.dart';
 
-/// A container that wraps a localized [exception] with a button that, when pressed, calls [onRetry].
+/// Wraps a localized [exception] with a button that, when pressed, calls [onRetry].
 ///
-/// This widget is mostly used in scenarios where a page couldn't load its essential data, meaning that there is no
-/// alternative other than retry loading its contents.
+/// Used in scenarios where a widget (almost always "pages") couldn't load its essential data.
 class ExceptionRetryContainer extends StatelessWidget {
   const ExceptionRetryContainer({required this.onRetry, required this.exception});
 
