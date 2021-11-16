2e6fc9af72c60cd8de98180b2799feda61bdadcd
Guilherme Girotto <guiga741@gmail.com>
2021-11-16 17:22:00 -0300

    Updates docs

--- lib/application/pages/details/details_providers.dart
+++ lib/application/pages/details/details_providers.dart
@@ -2,12 +2,10 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:memo/application/view-models/details/collection_details_vm.dart';
 
-/// Overridable collection id used in the scope of a collection details
-///
-/// Throws an [UnimplementedError] if the consuming context don't override this provider
+/// Throws an [UnimplementedError] if the consuming [ProviderScope] doesn't override this provider.
 final detailsCollectionId = Provider<String>((_) => throw UnimplementedError(), name: 'detailsCollectionId');
 
-/// Syntax sugar for watching [CollectionDetailsState] state updates.
+/// Syntax sugar for watching [collectionDetailsVM] state, using the scoped [detailsCollectionId].
 CollectionDetailsState watchCollectionDetailsState(WidgetRef ref) {
   final collectionId = ref.watch(detailsCollectionId);
   return ref.watch(collectionDetailsVM(collectionId));
--- lib/application/pages/execution/execution_providers.dart
+++ lib/application/pages/execution/execution_providers.dart
@@ -2,18 +2,16 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:memo/application/view-models/execution/collection_execution_vm.dart';
 
-/// Overridable collection id used in the scope of a collection execution
-///
-/// Throws an [UnimplementedError] if the consuming context don't override this provider
+/// Throws an [UnimplementedError] if the consuming [ProviderScope] doesn't override this provider.
 final executionCollectionId = Provider<String>((_) => throw UnimplementedError(), name: 'executionCollectionId');
 
-/// Syntax sugar for `ref.watch` the current [CollectionExecutionVM] provider.
+/// Syntax sugar for reading [collectionExecutionVM], using currently-scoped [executionCollectionId].
 CollectionExecutionVM readExecutionVM(WidgetRef ref) {
   final collectionId = ref.read(executionCollectionId);
   return ref.read(collectionExecutionVM(collectionId).notifier);
 }
 
-/// Syntax sugar for watching for [CollectionExecutionState] state updates.
+/// Syntax sugar for watching [collectionExecutionVM] state, using the scoped [executionCollectionId].
 CollectionExecutionState watchCollectionExecutionState(WidgetRef ref) {
   final collectionId = ref.watch(executionCollectionId);
   return ref.watch(collectionExecutionVM(collectionId));
