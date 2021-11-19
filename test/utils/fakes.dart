bec3ff8cfe9a0aba16237c6d459c39dee7e073ea
Guilherme Girotto <guiga741@gmail.com>
2021-11-19 18:21:13 -0300

    Moves `MockCallbackFunction` to a dedicated file

--- test/utils/fakes.dart
+++ test/utils/fakes.dart
@@ -1,12 +1,6 @@
-import 'package:mocktail/mocktail.dart';
-
 final question = [
   {'question': '?'}
 ];
 final answer = [
   {'answer': '!'}
 ];
-
-class MockCallbackFunction extends Mock {
-  void call();
-}
