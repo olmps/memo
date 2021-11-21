d0608fa12c0df4491b64f3c56efc7448e9f98eca
Guilherme Girotto <guiga741@gmail.com>
2021-11-21 11:53:54 -0300

    Updates `HighlightableText` and `TagsField` tests

--- test/application/widgets/theme/highlightable_text_test.dart
+++ test/application/widgets/theme/highlightable_text_test.dart
@@ -5,59 +5,81 @@ import 'package:memo/application/widgets/theme/highlightable_text.dart';
 import '../../../utils/widget_pump.dart';
 
 void main() {
-  const fakeText = 'A fake text';
-  const fakeTextStyle = TextStyle(color: Colors.white);
-  const fakeHighlight = 'fake';
-  const fakeHighlightStyle = TextStyle(color: Colors.red);
-
-  testWidgets('should not style highlight prefix', (tester) async {
-    const highlightableText = HighlightableText(
-      text: fakeText,
-      textStyle: fakeTextStyle,
-      highlighted: fakeHighlight,
-      highlightedStyle: fakeHighlightStyle,
-    );
+  testWidgets('should use the default textStyle on anything other than the highlightable text', (tester) async {
     const expectedPrefixSpan = TextSpan(text: 'A ', style: fakeTextStyle);
+    const expectedSuffixSpan = TextSpan(text: ' text', style: fakeTextStyle);
 
-    await pumpProviderScoped(tester, highlightableText);
+    final textSpan = await _pumpHighlightedText(tester);
 
-    final richText = tester.widget(find.byType(RichText)) as RichText;
-    final textSpan = richText.text as TextSpan;
     final innerTextSpan = textSpan.children!.first as TextSpan;
     expect(innerTextSpan.children!.first, expectedPrefixSpan);
+    expect(innerTextSpan.children!.last, expectedSuffixSpan);
   });
 
-  testWidgets('should style highlight text', (tester) async {
-    const highlightableText = HighlightableText(
-      text: fakeText,
-      textStyle: fakeTextStyle,
-      highlighted: fakeHighlight,
-      highlightedStyle: fakeHighlightStyle,
-    );
-    const expectedPrefixSpan = TextSpan(text: 'fake', style: fakeHighlightStyle);
+  testWidgets('should use the highlightedStyle on highlighted text', (tester) async {
+    const expectedHighlightSpan = TextSpan(text: 'fake', style: fakeHighlightStyle);
+
+    final textSpan = await _pumpHighlightedText(tester);
+
+    final innerTextSpan = textSpan.children!.first as TextSpan;
+    expect(innerTextSpan.children![1], expectedHighlightSpan);
+  });
+
+  testWidgets('should not highlight when highlighted text is null', (tester) async {
+    const highlightableText = HighlightableText(text: fakeText, textStyle: fakeTextStyle);
+    const expectedUnhighlightedText = Text(fakeText, style: fakeTextStyle);
 
     await pumpProviderScoped(tester, highlightableText);
 
-    final richText = tester.widget(find.byType(RichText)) as RichText;
-    final textSpan = richText.text as TextSpan;
-    final innerTextSpan = textSpan.children!.first as TextSpan;
-    expect(innerTextSpan.children![1], expectedPrefixSpan);
+    final unhighlightedText = tester.widget(find.byType(Text)) as Text;
+    expect(unhighlightedText.toString(), expectedUnhighlightedText.toString());
   });
 
-  testWidgets('should not style highlight suffix', (tester) async {
+  testWidgets('should not highlight when the highlighted text does not exists in original text', (tester) async {
+    const fakeNonExistingText = 'Memo';
     const highlightableText = HighlightableText(
       text: fakeText,
       textStyle: fakeTextStyle,
-      highlighted: fakeHighlight,
+      highlighted: fakeNonExistingText,
       highlightedStyle: fakeHighlightStyle,
     );
-    const expectedPrefixSpan = TextSpan(text: ' text', style: fakeTextStyle);
+    const expectedUnhighlightedText = Text(fakeText, style: fakeTextStyle);
 
     await pumpProviderScoped(tester, highlightableText);
 
-    final richText = tester.widget(find.byType(RichText)) as RichText;
-    final textSpan = richText.text as TextSpan;
-    final innerTextSpan = textSpan.children!.first as TextSpan;
-    expect(innerTextSpan.children!.last, expectedPrefixSpan);
+    final unhighlightedText = tester.widget(find.byType(Text)) as Text;
+    expect(unhighlightedText.toString(), expectedUnhighlightedText.toString());
   });
+
+  testWidgets('should not consider case sensitive in highlighted text', (tester) async {
+    const fakeCaseSensitiveHighlight = 'fAkE';
+    const expectedHighlightSpan = TextSpan(text: fakeHighlight, style: fakeHighlightStyle);
+
+    final textSpan = await _pumpHighlightedText(tester, highlightedText: fakeCaseSensitiveHighlight);
+
+    final innerTextSpan = textSpan.children!.first as TextSpan;
+    expect(innerTextSpan.children![1], expectedHighlightSpan);
+  });
+}
+
+const fakeText = 'A fake text';
+const fakeTextStyle = TextStyle(color: Colors.white);
+const fakeHighlight = 'fake';
+const fakeHighlightStyle = TextStyle(color: Colors.red);
+
+/// Pumps [HighlightableText] using [tester] and returns the resulting [RichText.text].
+///
+/// Uses [highlightedText] as the highlighted text. When not specified, [fakeHighlight] will be used.
+Future<TextSpan> _pumpHighlightedText(WidgetTester tester, {String? highlightedText}) async {
+  final highlightableText = HighlightableText(
+    text: fakeText,
+    textStyle: fakeTextStyle,
+    highlighted: highlightedText ?? fakeHighlight,
+    highlightedStyle: fakeHighlightStyle,
+  );
+
+  await pumpProviderScoped(tester, highlightableText);
+
+  final richText = tester.widget(find.byType(RichText)) as RichText;
+  return richText.text as TextSpan;
 }
