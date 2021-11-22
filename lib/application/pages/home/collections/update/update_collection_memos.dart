84faf0fa725e0e3010329ea3363804d1e85021cb
Guilherme Girotto <guiga741@gmail.com>
2021-11-22 14:28:18 -0300

    Replaces `CustomPainter` with asset

--- lib/application/pages/home/collections/update/update_collection_memos.dart
+++ lib/application/pages/home/collections/update/update_collection_memos.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:layoutr/common_layout.dart';
 import 'package:memo/application/constants/dimensions.dart' as dimens;
+import 'package:memo/application/constants/images.dart' as images;
 import 'package:memo/application/constants/strings.dart' as strings;
 import 'package:memo/application/theme/theme_controller.dart';
 
@@ -27,18 +28,10 @@ class _CreateMemoEmptyState extends ConsumerWidget {
     final theme = ref.watch(themeController);
     final textTheme = Theme.of(context).textTheme;
 
-    final roundedPlusIcon = CustomPaint(
-      size: const Size(dimens.createMemoCtaSide, dimens.createMemoCtaSide),
-      painter: _CreateButtonPainter(
-        backgroundColor: theme.primarySwatch.shade400,
-        createColor: theme.neutralSwatch.shade800,
-      ),
-    );
-
     final createMemoCta = Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        roundedPlusIcon,
+        Image.asset(images.addCircleAsset, color: theme.primarySwatch.shade400),
         context.verticalBox(Spacing.small),
         Text(
           strings.newMemo.toUpperCase(),
@@ -62,47 +55,4 @@ class _CreateMemoEmptyState extends ConsumerWidget {
       ),
     );
   }
-}
-
-class _CreateButtonPainter extends CustomPainter {
-  _CreateButtonPainter({required this.backgroundColor, required this.createColor});
-
-  final Color backgroundColor;
-  final Color createColor;
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final backgroundPaint = Paint()..color = backgroundColor;
-    final bounds = Rect.fromLTWH(0, 0, size.width, size.height);
-
-    final backgroundCircle = Path()
-      ..addOval(
-        Rect.fromCircle(
-          center: bounds.center,
-          radius: bounds.height / 2,
-        ),
-      );
-
-    canvas.drawPath(backgroundCircle, backgroundPaint);
-
-    final stripsPainter = Paint()
-      ..color = createColor
-      ..strokeWidth = dimens.createMemoCtaButtonStrokeWidth
-      ..style = PaintingStyle.fill
-      ..strokeCap = StrokeCap.round;
-
-    final xCenter = size.width / 2;
-    final yCenter = size.height / 2;
-
-    final xQuarter = size.width / 3.5;
-    final yQuarter = size.height / 3.5;
-
-    canvas
-      ..drawLine(Offset(xCenter, yQuarter), Offset(xCenter, size.height - yQuarter), stripsPainter)
-      ..drawLine(Offset(xQuarter, yCenter), Offset(size.width - xQuarter, yCenter), stripsPainter);
-  }
-
-  @override
-  bool shouldRepaint(_CreateButtonPainter oldDelegate) =>
-      backgroundColor != oldDelegate.backgroundColor || createColor != oldDelegate.createColor;
 }
