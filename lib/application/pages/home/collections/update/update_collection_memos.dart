03a98795a659a34c8ab47d7dbd883fc17fd09d8a
Guilherme Girotto <guiga741@gmail.com>
2021-11-22 10:03:51 -0300

    Fixes analyzer issues

--- lib/application/pages/home/collections/update/update_collection_memos.dart
+++ lib/application/pages/home/collections/update/update_collection_memos.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class UpdateCollectionMemos extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.blue, child: Center(child: Text('Memos')));
+    return Container(color: Colors.blue, child: const Center(child: Text('Memos')));
   }
 }
