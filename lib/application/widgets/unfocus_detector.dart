b5392b88a99de8c11764576be784e71b80e0bad6
Guilherme Girotto <guiga741@gmail.com>
2021-11-23 09:32:44 -0300

    Improves `UnfocusDetector` docs and tests

--- lib/application/widgets/unfocus_detector.dart
+++ lib/application/widgets/unfocus_detector.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
-/// A wrapper Widget that request unfocus when tapping outside [child].
+/// A wrapper Widget that request unfocus when tapping non-interactable Widgets of [child].
+///
+/// The interaction with focusable Widgets from [child] will not be affected. The unfocus is only request when
+/// interacting with non-focusable Widgets, such as [Container]s, [Column]s, etc.
 class UnfocusDetector extends StatelessWidget {
   const UnfocusDetector({required this.child});
 
