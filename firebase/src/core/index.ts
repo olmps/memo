098a2cb7b01c0fe7839088c7020fe03cb68fdf33
Guilherme Girotto <guiga741@gmail.com>
2021-12-01 16:40:39 -0300

    Updates project to only use `tsconfig-paths` when running tests and scripts

--- firebase/src/core/index.ts
+++ firebase/src/core/index.ts
@@ -1,7 +1,3 @@
-import { registerTSPaths } from "./ts-paths";
-// Always make sure that this is called before any other import/declaration.
-registerTSPaths();
-
 import * as functions from "firebase-functions";
 
 export const dummyFunction = functions.https.onRequest(async (_, res) => {
