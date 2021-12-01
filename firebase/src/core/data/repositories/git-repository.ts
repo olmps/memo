ee79d02531b5fbbd55d0e007f00b57d9bc98a675
Guilherme Girotto <guiga741@gmail.com>
2021-12-01 15:32:13 -0300

    Adds `GitRepository`

---
+++ firebase/src/core/data/repositories/git-repository.ts
@@ -0,0 +1,31 @@
+import { ShellGateway } from "@data/gateways/shell-gateway";
+
+export class GitRepository {
+  readonly #shellGateway: ShellGateway;
+
+  constructor(shellGateway: ShellGateway) {
+    this.#shellGateway = shellGateway;
+  }
+
+  /** Returns the SHA hash from the last merge commit made in the current checkout branch. */
+  async lastMergeCommitHash(): Promise<string> {
+    return this.#shellGateway.run("git rev-list --min-parents=2 --max-count=1 HEAD");
+  }
+
+  /** Returns the SHA hash from the last commit made in the current checkout branch. */
+  async lastCommitHash(): Promise<string> {
+    return this.#shellGateway.run("git rev-parse HEAD");
+  }
+
+  /** Runs `git diff` shell command between `base` and `head` commit hashes. */
+  async gitDiff(base: string, head: string, options?: { nameStatus: boolean }): Promise<string> {
+    let command = "git diff";
+    if (options?.nameStatus) {
+      command += " --name-status";
+    }
+
+    command += ` ${base} ${head}`;
+
+    return this.#shellGateway.run(command);
+  }
+}
