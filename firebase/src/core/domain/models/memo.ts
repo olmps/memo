7377920f983eb325856e2741b70b115961253af0
Guilherme Girotto <guiga741@gmail.com>
2021-12-02 13:57:35 -0300

    Updates Memo entity

--- firebase/src/core/domain/models/memo.ts
+++ firebase/src/core/domain/models/memo.ts
@@ -1,16 +1,36 @@
 import { defaultMaxStringLength, validate } from "#utils/validate";
 import * as Joi from "joi";
 
-export interface Memo {
-  readonly id: string;
-  readonly question: Record<string, unknown>[];
-  readonly answer: Record<string, unknown>[];
+interface MemoContent {
+  insert: string;
+  attributes?: {
+    bold?: boolean;
+    italic?: boolean;
+    underline?: boolean;
+    codeBlock?: boolean;
+  };
 }
 
+export interface Memo {
+  readonly uniqueId: string;
+  readonly question: MemoContent[];
+  readonly answer: MemoContent[];
+}
+
+const memoContentValidationSchema = Joi.object({
+  insert: Joi.string().max(defaultMaxStringLength).required(),
+  attributes: Joi.object({
+    bold: Joi.boolean(),
+    italic: Joi.boolean(),
+    underline: Joi.boolean(),
+    codeBlock: Joi.boolean(),
+  }).min(1),
+});
+
 export const memoValidationSchema = Joi.object({
-  id: Joi.string().max(defaultMaxStringLength).required(),
-  question: Joi.array().items(Joi.object()).min(1).required(),
-  answer: Joi.array().items(Joi.object()).min(1).required(),
+  uniqueId: Joi.string().max(defaultMaxStringLength).required(),
+  question: Joi.array().items(memoContentValidationSchema).min(1).required(),
+  answer: Joi.array().items(memoContentValidationSchema).min(1).required(),
 });
 
 export function validateMemo(memo: Memo): void {
