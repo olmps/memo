df1bc9ea90effc04be66d4ba03cef5e3ddca5233
Guilherme Girotto <guiga741@gmail.com>
2021-12-03 16:26:20 -0300

    Updates docs

--- firebase/src/core/data/repositories/git-repository.ts
+++ firebase/src/core/data/repositories/git-repository.ts
@@ -1,6 +1,6 @@
 import { ShellGateway } from "@data/gateways/shell-gateway";
 
-/** Interfaces access to the project git repository information. */
+/** Git utilities applied to the current node process root folder. */
 export class GitRepository {
   readonly #shellGateway: ShellGateway;
 
