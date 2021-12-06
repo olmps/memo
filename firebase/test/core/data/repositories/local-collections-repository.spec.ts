9facb626e269d0ecc3c8ed7f73347ffdc83d12bd
Guilherme Girotto <guiga741@gmail.com>
2021-12-06 15:14:18 -0300

    Improves tests descriptions and uses `createSinonStub` aux function

--- firebase/test/core/data/repositories/local-collections-repository.spec.ts
+++ firebase/test/core/data/repositories/local-collections-repository.spec.ts
@@ -1,44 +1,40 @@
 import * as assert from "assert";
-import * as sinon from "sinon";
 import { FileSystemGateway } from "#data/gateways/filesystem-gateway";
 import { SchemaValidator } from "#data/schemas/schema-validator";
 import { LocalCollectionsRepository } from "#data/repositories/local-collections-repository";
 import FilesystemError from "#faults/errors/filesystem-error";
 import SerializationError from "#faults/errors/serialization-error";
 import { LocalPublicCollection } from "#domain/models/collection";
+import createSinonStub from "#test/sinon-stub";
 
 describe("LocalCollectionsRepository", () => {
-  const fsGateway = sinon.createStubInstance(FileSystemGateway);
-  const schemaValidator = sinon.createStubInstance(SchemaValidator);
-  const localCollectionsRepo = new LocalCollectionsRepository(
-    <FileSystemGateway>(<any>fsGateway),
-    <SchemaValidator>(<any>schemaValidator),
-    "any"
-  );
+  const fsGatewayStub = createSinonStub(FileSystemGateway);
+  const schemaValidatorStub = createSinonStub(SchemaValidator);
+  const localCollectionsRepo = new LocalCollectionsRepository(fsGatewayStub, schemaValidatorStub, "any");
 
   describe("getAllCollectionsByIds", () => {
     it("should return local stored collections", async () => {
-      fsGateway.readFileAsString.resolves(JSON.stringify(mockLocalCollectionJson));
+      fsGatewayStub.readFileAsString.resolves(JSON.stringify(mockLocalCollectionJson));
 
       const localCollections = await localCollectionsRepo.getAllCollectionsByIds(["any"]);
 
       assert.deepStrictEqual(localCollections[0], <LocalPublicCollection>mockLocalCollectionJson);
     });
 
-    it("should fail to retrieve collections when file system read fails", async () => {
-      const fsError = new FilesystemError({ message: "Error Message" });
+    it("should throw when reading files from system fails", async () => {
+      const mockFsError = new FilesystemError({ message: "Error Message" });
 
-      fsGateway.readFileAsString.rejects(fsError);
+      fsGatewayStub.readFileAsString.rejects(mockFsError);
 
-      assert.rejects(() => localCollectionsRepo.getAllCollectionsByIds([]), fsError);
+      assert.rejects(() => localCollectionsRepo.getAllCollectionsByIds([]), FilesystemError);
     });
 
-    it("should fail to retrieve collections when de-serialization fails", async () => {
-      const serializationError = new SerializationError({ message: "Error Message" });
+    it("should throw when collection de-serialization fails", async () => {
+      const mockSerializationError = new SerializationError({ message: "Error Message" });
 
-      schemaValidator.validateObject.throws(serializationError);
+      schemaValidatorStub.validateObject.throws(mockSerializationError);
 
-      assert.rejects(() => localCollectionsRepo.getAllCollectionsByIds([]), serializationError);
+      assert.rejects(() => localCollectionsRepo.getAllCollectionsByIds([]), SerializationError);
     });
   });
 });
