813cfbe1083ee0542c93422b31537c45e50fc0c3
Guilherme Girotto <guiga741@gmail.com>
2021-12-07 16:00:52 -0300

    Adds type-checking to gateways tests

--- firebase/test/core/data/gateways/shell-gateway.spec.ts
+++ firebase/test/core/data/gateways/shell-gateway.spec.ts
@@ -5,15 +5,12 @@ import { ShellGateway } from "#data/gateways/shell-gateway";
 import ShellError from "#faults/errors/shell-error";
 
 describe("ShellGateway", () => {
-  // TODO(matuella): How to mock while maintaining types?
-  // This way, we won't have to call `fsMock.expect("unsafeMethodCall")` and could
-  // possibly use `createSinonStub`
   let sandbox: sinon.SinonSandbox;
-  let childProcessMock: sinon.SinonMock;
+  let childProcessStub: sinon.SinonStubbedInstance<typeof child_process>;
 
   beforeEach(() => {
     sandbox = sinon.createSandbox();
-    childProcessMock = sandbox.mock(child_process);
+    childProcessStub = sandbox.stub(child_process);
   });
 
   afterEach(() => {
@@ -23,7 +20,7 @@ describe("ShellGateway", () => {
   it("should reject when exec returns an error", async () => {
     const shellGateway = new ShellGateway();
 
-    childProcessMock.expects("exec").yields("Exception", "", "");
+    childProcessStub.exec.yields("Exception", "", "");
 
     await assert.rejects(() => shellGateway.run("any"), ShellError);
   });
@@ -31,7 +28,7 @@ describe("ShellGateway", () => {
   it("should reject when exec stderr is not empty", async () => {
     const shellGateway = new ShellGateway();
 
-    childProcessMock.expects("exec").yields(null, "", "Exception");
+    childProcessStub.exec.yields(null, "", "Exception");
 
     await assert.rejects(() => shellGateway.run("any"), ShellError);
   });
@@ -40,7 +37,7 @@ describe("ShellGateway", () => {
     const expectedOutput = "Output";
     const shellGateway = new ShellGateway();
 
-    childProcessMock.expects("exec").yields(null, expectedOutput, "");
+    childProcessStub.exec.yields(null, expectedOutput, "");
     const output = await shellGateway.run("any");
 
     assert.strictEqual(output, expectedOutput);
