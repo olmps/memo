11ece8d4bd2fc8a4a835575bb6db01f089115238
Guilherme Girotto <guiga741@gmail.com>
2021-12-07 13:56:53 -0300

    Updates schema and entity validations to use shared validator

--- firebase/test/core/domain/models/memo.spec.ts
+++ firebase/test/core/domain/models/memo.spec.ts
@@ -1,159 +1,53 @@
 import ValidationError from "#faults/errors/validation-error";
-import { Memo, memoQuillValidationSchema, memoValidationSchema } from "#domain/models/memo";
+import { memoQuillValidationSchema, memoValidationSchema } from "#domain/models/memo";
 import { throws } from "assert";
 import { defaultMaxStringLength, validate } from "#utils/validate";
+import { EntityValidatorBuilder, ValidationProperties } from "#test/validator";
+import { newRawMemo, newRawMemoContent } from "#test/core/data/schemas/collections-fakes";
 
 describe("Memo Content Validation", () => {
-  describe("Base Properties", () => {
-    describe("Id - ", () => {
-      it("should throw when id is not set", () => {
-        const fakeMemo = {
-          question: [{ insert: "any" }],
-          answer: [{ insert: "any" }],
-        };
+  describe("Root Properties", () => {
+    const properties: ValidationProperties = {
+      required: ["id", "question", "answer"],
+      array: ["question", "answer"],
+      uniqueItems: new Map<string, any[]>([
+        ["question", [newRawMemoContent(), newRawMemoContent()]],
+        ["answer", [newRawMemoContent(), newRawMemoContent()]],
+      ]),
+      incorrectTypes: new Map<string, any>([
+        ["id", true],
+        ["question", "string"],
+        ["answer", "string"],
+      ]),
+      lengthRestricted: new Map<string, any>([["id", defaultMaxStringLength]]),
+    };
 
-        throws(() => validate(memoValidationSchema, fakeMemo), ValidationError);
-      });
-
-      it("should throw when id is empty", () => {
-        const fakeMemo: Memo = {
-          id: "",
-          question: [{ insert: "any" }],
-          answer: [{ insert: "any" }],
-        };
-
-        throws(() => validate(memoValidationSchema, fakeMemo), ValidationError);
-      });
-
-      it("should throw when id is greater than max allowed string length", () => {
-        const fakeId = "a".repeat(defaultMaxStringLength + 1);
-        const fakeMemo: Memo = {
-          id: fakeId,
-          question: [{ insert: "any" }],
-          answer: [{ insert: "any" }],
-        };
-
-        throws(() => validate(memoValidationSchema, fakeMemo), ValidationError);
-      });
-
-      it("should throw when id is not a string", () => {
-        const fakeMemo = {
-          id: true,
-          question: [{ insert: "any" }],
-          answer: [{ insert: "any" }],
-        };
-
-        throws(() => validate(memoValidationSchema, fakeMemo), ValidationError);
-      });
+    const validator = new EntityValidatorBuilder({
+      schema: memoValidationSchema,
+      entityConstructor: newRawMemo,
+      properties: properties,
     });
 
-    describe("Question - ", () => {
-      it("should throw when question is not set", () => {
-        const fakeMemo = {
-          id: "any",
-          answer: [{ insert: "any" }],
-        };
-
-        throws(() => validate(memoValidationSchema, fakeMemo), ValidationError);
-      });
-
-      it("should throw when question is empty", () => {
-        const fakeMemo = {
-          id: "any",
-          question: [],
-          answer: [{ insert: "any" }],
-        };
-
-        throws(() => validate(memoValidationSchema, fakeMemo), ValidationError);
-      });
-
-      it("should throw when question is not an array", () => {
-        const fakeMemo = {
-          id: "any",
-          question: true,
-          answer: [{ insert: "any" }],
-        };
-
-        throws(() => validate(memoValidationSchema, fakeMemo), ValidationError);
-      });
-    });
-
-    describe("Answer - ", () => {
-      it("should throw when answer is not set", () => {
-        const fakeMemo = {
-          id: "any",
-          question: [{ insert: "any" }],
-        };
-
-        throws(() => validate(memoValidationSchema, fakeMemo), ValidationError);
-      });
-
-      it("should throw when answer is empty", () => {
-        const fakeMemo = {
-          id: "any",
-          question: [{ insert: "any" }],
-          answer: [],
-        };
-
-        throws(() => validate(memoValidationSchema, fakeMemo), ValidationError);
-      });
-
-      it("should throw when answer is not an array", () => {
-        const fakeMemo = {
-          id: "any",
-          question: [{ insert: "any" }],
-          answer: true,
-        };
-
-        throws(() => validate(memoValidationSchema, fakeMemo), ValidationError);
-      });
-    });
+    validator.validate();
   });
 
   describe("Content Properties - ", () => {
-    describe("Question - ", () => {
-      it("should throw when insert is not set", () => {
-        const fakeMemo = {
-          id: "any",
-          question: [{}],
-          answer: [{ insert: "any" }],
-        };
+    const properties: ValidationProperties = {
+      required: ["insert"],
+      optional: ["attributes"],
+      incorrectTypes: new Map<string, any>([
+        ["insert", 0],
+        ["attributes", "string"],
+      ]),
+    };
 
-        throws(() => validate(memoValidationSchema, fakeMemo), ValidationError);
-      });
-
-      it("should throw when insert is empty", () => {
-        const fakeMemo = {
-          id: "any",
-          question: [{ insert: "" }],
-          answer: [{ insert: "any" }],
-        };
-
-        throws(() => validate(memoValidationSchema, fakeMemo), ValidationError);
-      });
+    const validator = new EntityValidatorBuilder({
+      schema: memoQuillValidationSchema,
+      entityConstructor: newRawMemoContent,
+      properties: properties,
     });
 
-    describe("Answer - ", () => {
-      it("should throw when insert is not set", () => {
-        const fakeMemo = {
-          id: "any",
-          question: [{ insert: "any" }],
-          answer: [{}],
-        };
-
-        throws(() => validate(memoValidationSchema, fakeMemo), ValidationError);
-      });
-
-      it("should throw when insert is empty", () => {
-        const fakeMemo = {
-          id: "any",
-          question: [{ insert: "any" }],
-          answer: [{ insert: "" }],
-        };
-
-        throws(() => validate(memoValidationSchema, fakeMemo), ValidationError);
-      });
-    });
+    validator.validate();
 
     describe("Attributes - ", () => {
       it("should throw when attributes is empty", () => {
