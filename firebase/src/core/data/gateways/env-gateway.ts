bde746f389a98c9322d669cb572347e59246305d
Guilherme Girotto <guiga741@gmail.com>
2021-12-07 18:10:42 -0300

    Adds `FirebaseServiceAccountMetadata` to `EnvGateway`

--- firebase/src/core/data/gateways/env-gateway.ts
+++ firebase/src/core/data/gateways/env-gateway.ts
@@ -1,7 +1,38 @@
+type ConfigKeys = "credentials";
+type Config = Record<ConfigKeys, unknown>;
+
+type CredentialsKeys = "project_id" | "client_email" | "private_key";
+type Credentials = Record<CredentialsKeys, string>;
+
+interface FirebaseServiceAccountMetadata {
+  projectId: string;
+  clientEmail: string;
+  privateKey: string;
+}
+
 /** Exposes environment config variables. */
 export class EnvGateway {
+  readonly #config: Config;
+
+  constructor(config: Record<string, unknown>) {
+    this.#config = config as Config;
+  }
+
   /** Returns `true` if running in local environment, i.e, under Firebase Emulators. */
   get isLocalDevelopment(): boolean {
     return process.env["FUNCTIONS_EMULATOR"] === "true";
+  }
+
+  get firebaseServiceAccount(): FirebaseServiceAccountMetadata {
+    const credentialsConfig = this.#config["credentials"] as Credentials;
+    const projectId = credentialsConfig["project_id"];
+    const clientEmail = credentialsConfig["client_email"];
+    const privateKey = credentialsConfig["private_key"];
+
+    return {
+      projectId,
+      clientEmail,
+      privateKey,
+    };
   }
 }
--- firebase/src/core/data/gateways/firebase-app.ts
+++ firebase/src/core/data/gateways/firebase-app.ts
@@ -1,9 +1,18 @@
 import * as admin from "firebase-admin";
+import { EnvGateway } from "./env-gateway";
 
-export function initializeFirebase(options: { isLocalDevelopment: boolean }): admin.app.App {
-  if (options.isLocalDevelopment) {
+export function initializeFirebase(env: EnvGateway): admin.app.App {
+  if (env.isLocalDevelopment) {
     process.env["FIRESTORE_EMULATOR_HOST"] = "localhost:8080";
   }
 
-  return admin.initializeApp();
+  const serviceAccount = env.firebaseServiceAccount;
+  return admin.initializeApp({
+    projectId: serviceAccount.projectId,
+    credential: admin.credential.cert({
+      clientEmail: serviceAccount.clientEmail,
+      projectId: serviceAccount.projectId,
+      privateKey: serviceAccount.privateKey,
+    }),
+  });
 }
