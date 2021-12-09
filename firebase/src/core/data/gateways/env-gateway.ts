2a2eea1ebf52ec0351d92526f1afa85571fd5865
Guilherme Girotto <guiga741@gmail.com>
2021-12-09 17:08:40 -0300

    Adds script execution launch

--- firebase/src/core/data/gateways/env-gateway.ts
+++ firebase/src/core/data/gateways/env-gateway.ts
@@ -20,7 +20,7 @@ export class EnvGateway {
 
   /** Returns `true` if running in local environment, i.e, under Firebase Emulators. */
   get isLocalDevelopment(): boolean {
-    return process.env["FUNCTIONS_EMULATOR"] === "true";
+    return process.env["FUNCTIONS_EMULATOR"] === "true" || process.env["ENVIRONMENT"] === "DEV";
   }
 
   get firebaseServiceAccount(): FirebaseServiceAccountMetadata {
