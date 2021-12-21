0fe7f636a89e6ffcd12d4fb4ee887b2936e24471
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-12-21 11:49:11 -0300

    Update `data/serializers` to conform to server structure

--- flutter/lib/data/serializers/collection_serializer.dart
+++ flutter/lib/data/serializers/collection_serializer.dart
@@ -1,57 +1,60 @@
-import 'package:memo/data/serializers/contributor_serializer.dart';
-import 'package:memo/data/serializers/memo_difficulty_parser.dart';
 import 'package:memo/data/serializers/serializer.dart';
+import 'package:memo/domain/enums/resource_type.dart';
 import 'package:memo/domain/models/collection.dart';
 
 class CollectionKeys {
   static const id = 'id';
   static const name = 'name';
-  static const description = 'description';
   static const category = 'category';
+  static const description = 'description';
+  static const locale = 'locale';
   static const tags = 'tags';
-  static const uniqueMemosAmount = 'uniqueMemosAmount';
-  static const uniqueMemoExecutionsAmount = 'uniqueMemoExecutionsAmount';
-  static const executionsAmounts = 'executionsAmounts';
-  static const timeSpentInMillis = 'timeSpentInMillis';
+
+  static const memosAmount = 'memosAmount';
+  static const memosOrder = 'memosOrder';
+
   static const contributors = 'contributors';
+  static const resources = 'resources';
 }
 
 class CollectionSerializer implements Serializer<Collection, Map<String, dynamic>> {
   final contributorSerializer = ContributorSerializer();
+  final resourceSerializer = ResourceSerializer();
 
   @override
   Collection from(Map<String, dynamic> json) {
     final id = json[CollectionKeys.id] as String;
     final name = json[CollectionKeys.name] as String;
-    final description = json[CollectionKeys.description] as String;
-    final category = json[CollectionKeys.category] as String;
 
-    final rawTags = json[CollectionKeys.tags] as List;
-    final tags = List<String>.from(rawTags);
+    final memosAmount = json[CollectionKeys.memosAmount] as int;
 
-    final uniqueMemosAmount = json[CollectionKeys.uniqueMemosAmount] as int;
-    final uniqueMemoExecutionsAmount = json[CollectionKeys.uniqueMemoExecutionsAmount] as int?;
+    final rawMemosOrder = json[CollectionKeys.memosOrder] as List;
+    final memosOrder = List<String>.from(rawMemosOrder);
 
-    final rawExecutionsAmounts = json[CollectionKeys.executionsAmounts] as Map<String, dynamic>?;
-    final executionsAmounts =
-        rawExecutionsAmounts?.map((key, dynamic value) => MapEntry(memoDifficultyFromRaw(key), value as int));
+    final category = json[CollectionKeys.category] as String?;
+    final description = json[CollectionKeys.description] as String?;
+    final locale = json[CollectionKeys.description] as String?;
 
-    final timeSpentInMillis = json[CollectionKeys.timeSpentInMillis] as int?;
+    final rawTags = json[CollectionKeys.tags] as List?;
+    final tags = rawTags != null ? List<String>.from(rawTags) : null;
 
     final rawContributors = List<Map<String, dynamic>>.from(json[CollectionKeys.contributors] as List);
     final contributors = rawContributors.map(contributorSerializer.from).toList();
+
+    final rawResources = List<Map<String, dynamic>>.from(json[CollectionKeys.resources] as List);
+    final resources = rawResources.map(resourceSerializer.from).toList();
 
     return Collection(
       id: id,
       name: name,
       description: description,
       category: category,
+      locale: locale,
       tags: tags,
-      uniqueMemosAmount: uniqueMemosAmount,
-      uniqueMemoExecutionsAmount: uniqueMemoExecutionsAmount ?? 0,
-      executionsAmounts: executionsAmounts ?? {},
-      timeSpentInMillis: timeSpentInMillis ?? 0,
+      memosAmount: memosAmount,
+      memosOrder: memosOrder,
       contributors: contributors,
+      resources: resources,
     );
   }
 
@@ -59,13 +62,92 @@ class CollectionSerializer implements Serializer<Collection, Map<String, dynamic
   Map<String, dynamic> to(Collection collection) => <String, dynamic>{
         CollectionKeys.id: collection.id,
         CollectionKeys.name: collection.name,
-        CollectionKeys.description: collection.description,
-        CollectionKeys.category: collection.category,
-        CollectionKeys.tags: collection.tags,
-        CollectionKeys.uniqueMemosAmount: collection.uniqueMemosAmount,
-        CollectionKeys.uniqueMemoExecutionsAmount: collection.uniqueMemoExecutionsAmount,
-        CollectionKeys.executionsAmounts: collection.executionsAmounts.map((key, value) => MapEntry(key.raw, value)),
-        CollectionKeys.contributors: collection.contributors.map(contributorSerializer.to),
-        CollectionKeys.timeSpentInMillis: collection.timeSpentInMillis,
+        CollectionKeys.memosAmount: collection.memosAmount,
+        CollectionKeys.memosOrder: collection.memosOrder,
+        if (collection.description != null) CollectionKeys.description: collection.description,
+        if (collection.category != null) CollectionKeys.category: collection.category,
+        if (collection.locale != null) CollectionKeys.locale: collection.locale,
+        if (collection.tags != null) CollectionKeys.tags: collection.tags,
+        if (collection.contributors != null)
+          CollectionKeys.contributors: collection.contributors!.map(contributorSerializer.to),
+        if (collection.resources != null) CollectionKeys.resources: collection.resources!.map(resourceSerializer.to),
+      };
+}
+
+class ResourceKeys {
+  static const id = 'id';
+  static const description = 'description';
+  static const type = 'type';
+  static const url = 'url';
+}
+
+class ResourceSerializer implements Serializer<Resource, Map<String, dynamic>> {
+  @override
+  Resource from(Map<String, dynamic> json) {
+    final id = json[ResourceKeys.id] as String;
+    final description = json[ResourceKeys.description] as String;
+    final rawType = json[ResourceKeys.type] as String;
+    final type = _typeFromRaw(rawType);
+
+    final url = json[ResourceKeys.url] as String;
+
+    return Resource(
+      id: id,
+      description: description,
+      type: type,
+      url: url,
+    );
+  }
+
+  @override
+  Map<String, dynamic> to(Resource resource) => <String, dynamic>{
+        ResourceKeys.id: resource.id,
+        ResourceKeys.description: resource.description,
+        ResourceKeys.type: resource.type.raw,
+        ResourceKeys.url: resource.url,
+      };
+
+  ResourceType _typeFromRaw(String raw) => ResourceType.values.firstWhere(
+        (type) => type.raw == raw,
+        orElse: () => ResourceType.unknown,
+      );
+}
+
+extension on ResourceType {
+  String get raw {
+    switch (this) {
+      case ResourceType.article:
+        return 'article';
+      case ResourceType.book:
+        return 'book';
+      case ResourceType.video:
+        return 'video';
+      case ResourceType.unknown:
+        return 'unknown';
+    }
+  }
+}
+
+class ContributorKeys {
+  static const name = 'name';
+  static const url = 'url';
+  static const imageUrl = 'imageUrl';
+}
+
+class ContributorSerializer implements Serializer<Contributor, Map<String, dynamic>> {
+  @override
+  Contributor from(Map<String, dynamic> json) {
+    final name = json[ContributorKeys.name] as String;
+    final url = json[ContributorKeys.url] as String?;
+    final imageUrl = json[ContributorKeys.imageUrl] as String?;
+
+    return Contributor(name: name, url: url, imageUrl: imageUrl);
+  }
+
+  @override
+  Map<String, dynamic> to(Contributor contributor) => <String, dynamic>{
+        ContributorKeys.name: contributor.name,
+        if (contributor.url != null) ContributorKeys.url: contributor.url,
+        if (contributor.imageUrl != null) ContributorKeys.imageUrl: contributor.imageUrl,
       };
 }
--- flutter/lib/data/serializers/memo_execution_serializer.dart
+++ flutter/lib/data/serializers/memo_execution_serializer.dart
@@ -6,49 +6,49 @@ class MemoExecutionKeys {
   static const collectionId = 'collectionId';
   static const started = 'started';
   static const finished = 'finished';
-  static const uniqueId = 'uniqueId';
-  static const rawQuestion = 'question';
-  static const rawAnswer = 'answer';
+  static const id = 'id';
+  static const question = 'question';
+  static const answer = 'answer';
   static const markedDifficulty = 'markedDifficulty';
 }
 
 class MemoExecutionSerializer implements Serializer<MemoExecution, Map<String, dynamic>> {
   @override
   MemoExecution from(Map<String, dynamic> json) {
-    final uniqueId = json[MemoExecutionKeys.uniqueId] as String;
-    final collectionId = json[MemoExecutionKeys.collectionId] as String;
+    final id = json[MemoExecutionKeys.id] as String;
 
+    // TODO(matuella): Should we store as String or millis since epoch using UTC?
     final rawStarted = json[MemoExecutionKeys.started] as int;
     final started = DateTime.fromMillisecondsSinceEpoch(rawStarted, isUtc: true);
-
+    // TODO(matuella): Should we store as String or millis since epoch using UTC?
     final rawFinished = json[MemoExecutionKeys.finished] as int;
     final finished = DateTime.fromMillisecondsSinceEpoch(rawFinished, isUtc: true);
 
-    final rawQuestion = List<Map<String, dynamic>>.from(json[MemoExecutionKeys.rawQuestion] as List);
-    final rawAnswer = List<Map<String, dynamic>>.from(json[MemoExecutionKeys.rawAnswer] as List);
+    final question = List<Map<String, dynamic>>.from(json[MemoExecutionKeys.question] as List);
+    final answer = List<Map<String, dynamic>>.from(json[MemoExecutionKeys.answer] as List);
 
     final rawDifficulty = json[MemoExecutionKeys.markedDifficulty] as String;
     final markedDifficulty = memoDifficultyFromRaw(rawDifficulty);
 
     return MemoExecution(
-      collectionId: collectionId,
+      id: id,
+      question: question,
+      answer: answer,
       started: started,
       finished: finished,
-      uniqueId: uniqueId,
-      rawQuestion: rawQuestion,
-      rawAnswer: rawAnswer,
       markedDifficulty: markedDifficulty,
     );
   }
 
   @override
   Map<String, dynamic> to(MemoExecution execution) => <String, dynamic>{
-        MemoExecutionKeys.collectionId: execution.collectionId,
+        MemoExecutionKeys.id: execution.id,
+        // TODO(matuella): Should we store as String or millis since epoch using UTC?
         MemoExecutionKeys.started: execution.started.toUtc().millisecondsSinceEpoch,
+        // TODO(matuella): Should we store as String or millis since epoch using UTC?
         MemoExecutionKeys.finished: execution.finished.toUtc().millisecondsSinceEpoch,
-        MemoExecutionKeys.uniqueId: execution.uniqueId,
-        MemoExecutionKeys.rawQuestion: execution.rawQuestion,
-        MemoExecutionKeys.rawAnswer: execution.rawAnswer,
+        MemoExecutionKeys.question: execution.question,
+        MemoExecutionKeys.answer: execution.answer,
         MemoExecutionKeys.markedDifficulty: execution.markedDifficulty.raw,
       };
 }
