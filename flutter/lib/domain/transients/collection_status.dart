823c5d584d9651506c6f15039108554255d81487
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2022-02-09 09:06:02 -0300

    Add `isPrivate` property to `CollectionExecution`

--- flutter/lib/domain/transients/collection_status.dart
+++ flutter/lib/domain/transients/collection_status.dart
@@ -1,13 +1,20 @@
 import 'package:memo/domain/models/collection.dart';
+import 'package:memo/domain/models/collection_execution.dart';
 
 /// Groups a [Collection] with its memory recall.
 class CollectionStatus {
-  CollectionStatus(this.collection, this.memoryRecall);
+  CollectionStatus(this.collection, [this.execution, this.memoryRecall])
+      : assert(
+          (execution?.isCompleted ?? false) ^ (memoryRecall == null),
+          'When execution is completed, an associated memoryRecall is required',
+        );
 
   final Collection collection;
 
-  /// Average of all [collection]'s memos memory recall.
+  final CollectionExecution? execution;
+
+  /// Average of all [execution]'s memos memory recall.
   ///
-  /// Should be `null` if [Collection.isCompleted] is `false`.
+  /// Should be `null` if  [CollectionExecution.isCompleted] `false`.
   final double? memoryRecall;
 }
